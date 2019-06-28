170f32087023e935448988abf7b752d8e08f0e57
07akioni <07akioni2@gmail.com>
2019-06-28 15:41:29 +0800

    chore: style-lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   "extends": "stylelint-config-recommended-scss",
   "rules": {
-    'no-descending-specificity': null
+    'no-descending-specificity': null,
+    "indentation": 2,
+    "max-empty-lines": 1
   }
 }
