d954af9c792fdb2a9f4c5b506297338b1e4cacb4
07akioni <07akioni2@gmail.com>
2019-08-06 17:48:39 +0800

    feat(checkbox): add checkbox-group

---
+++ demo/components/checkboxDemo/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    ref="doc"
+    class="n-doc"
+  >
+    <div class="n-doc-header">
+      <n-gradient-text :font-size="20">
+        scaffold
+      </n-gradient-text>
+    </div>
+    <div class="n-doc-body">
+      <scaffold />
+      <checkbox-group />
+    </div>
+  </div>
+</template>
+
+<script>
+import scaffold from './scaffold.demo.vue'
+import checkboxGroup from './checkboxGroup.demo'
+
+export default {
+  components: {
+    scaffold,
+    checkboxGroup
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
--- packages/common/Checkbox/index.js
+++ packages/common/Checkbox/index.js
@@ -1,8 +1,10 @@
 /* istanbul ignore file */
 import NCheckbox from './src/main.vue'
+import NCheckboxGroup from './src/CheckboxGroup.vue'
 
 NCheckbox.install = function (Vue) {
   Vue.component(NCheckbox.name, NCheckbox)
+  Vue.component(NCheckboxGroup.name, NCheckboxGroup)
 }
 
 export default NCheckbox
