7d8b701fcf67eac4e0455867c6b73b4462dd239f
07akioni <07akioni2@gmail.com>
2019-08-14 19:26:40 +0800

    refactor(switch): change size slightly

---
+++ demo/components/switchDemo/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    ref="doc"
+    class="n-doc"
+  >
+    <div class="n-doc-header">
+      <n-gradient-text :font-size="20">
+        Switch / n-switch
+      </n-gradient-text>
+    </div>
+    <div class="n-doc-body">
+      <basic-usage />
+      <change-event />
+    </div>
+  </div>
+</template>
+
+<script>
+import basicUsage from './basicUsage.demo.vue'
+import changeEvent from './changeEvent.demo.vue'
+
+export default {
+  components: {
+    basicUsage,
+    changeEvent
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+  }
+}
+</script>
