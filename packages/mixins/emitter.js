0aa560c924be48fa1e6dc647bb2c42dbd1cb1b1c
07akioni <07akioni2@gmail.com>
2019-08-16 16:43:08 +0800

    style(mixins/emitter): remove empty lines

--- packages/mixins/emitter.js
+++ packages/mixins/emitter.js
@@ -14,10 +14,8 @@ export default {
     dispatch (componentName, eventName, params) {
       let parent = this.$parent || this.$root
       let name = parent.$options.name
-
       while (parent && (!name || name !== componentName)) {
         parent = parent.$parent
-
         if (parent) {
           name = parent.$options.name
         }
