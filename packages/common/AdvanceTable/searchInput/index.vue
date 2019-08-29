d0b69fdd309b9080aa534256ea97de46a4d5fe3f
JiwenBai <jiwen.bai@tusimple.ai>
2019-08-29 15:12:58 +0800

    fixed(advance-table): clearSearch without search columns

--- packages/common/AdvanceTable/searchInput/index.vue
+++ packages/common/AdvanceTable/searchInput/index.vue
@@ -42,7 +42,7 @@ export default {
   methods: {
     clearSearch () {
       this.value = ''
-      this.selectedValue = this.options.columns[0].value
+      this.selectedValue = this.options.columns ? this.options.columns[0].value : ''
       this.handleInputChange()
     },
     setSearch ({ key, value }) {
