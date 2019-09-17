2002eb87529a28326708f257256328113e39d0f8
07akioni <07akioni2@gmail.com>
2019-09-17 19:29:27 +0800

    feat(mixins/themeable): support theme

---
+++ packages/mixins/themeable.js
@@ -0,0 +1,17 @@
+export default {
+  props: {
+    theme: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    synthesizedTheme () {
+      if (this.theme !== null) {
+        return this.theme
+      } else {
+        return (this.NApp && this.NApp.theme) || 'default'
+      }
+    }
+  }
+}
