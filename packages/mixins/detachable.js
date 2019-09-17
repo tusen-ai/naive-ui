d06448d385c7495e0b7258fb44714f6ea94c56da
07akioni <07akioni2@gmail.com>
2019-09-17 19:27:48 +0800

    refactor(mixins/detachable):  use proper implementation to support  namespace

--- packages/mixins/detachable.js
+++ packages/mixins/detachable.js
@@ -1,3 +1,5 @@
+import withapp from './withapp'
+
 /**
  * Detach $refs.contentContainer to detachTarget
  *
@@ -7,16 +9,7 @@
  * @prop {HTMLElement} detachTarget determine where should $refs.contentContainer to be detached
  */
 export default {
-  inject: {
-    NApp: {
-      default: null
-    }
-  },
-  computed: {
-    namespace () {
-      return (this.NApp && this.NApp.namespace) || null
-    }
-  },
+  mixins: [withapp],
   props: {
     detachTarget: {
       validator () {
@@ -29,30 +22,10 @@ export default {
       default: false
     }
   },
-  watch: {
-    'NApp.namespace': function (newNamespace, oldNamespace) {
-      this.removeNamespace(oldNamespace)
-      this.setNamespace(newNamespace)
-    }
-  },
   methods: {
     detachContent () {
       this.$refs.contentContainer.parentNode.removeChild(this.$refs.contentContainer)
       this.detachTarget.append(this.$refs.contentContainer)
-    },
-    removeNamespace (namespace) {
-      if (!namespace) return
-      const contentContainer = this.$refs.contentContainer
-      if (contentContainer) {
-        contentContainer.classList.remove(namespace)
-      }
-    },
-    setNamespace (namespace) {
-      if (!namespace) return
-      const contentContainer = this.$refs.contentContainer
-      if (contentContainer) {
-        contentContainer.classList.add(namespace)
-      }
     }
   },
   beforeMount () {
@@ -60,22 +33,9 @@ export default {
       console.warn(this.$options.name, ' will be mounted to', this.detachTarget, ', but it doesn\'t exist! Modal component won\'t work!')
     }
   },
-  /**
-   * There might be some potential problem, it may exists some moment that namespace is removed
-   */
   mounted () {
     this.detachTarget = document.body // getScrollParent(this.$refs.self)
-    if (this.namespace) {
-      this.setNamespace(this.namespace)
-    }
     this.detachContent()
-  },
-  updated () {
-    if (this.namespace) {
-      this.$nextTick().then(() => {
-        this.setNamespace(this.namespace)
-      })
-    }
   },
   beforeDestroy () {
     if (!this.cleanManually) {
