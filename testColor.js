85d6e877248f6e5178db0c08bbde1d9ab6050990
07akioni <07akioni2@gmail.com>
2019-09-18 22:01:34 +0800

    refactor(button): color

---
+++ testColor.js
@@ -0,0 +1,7 @@
+const tinyColor = require('tinycolor2')
+
+const red = tinyColor('#ce347c').getBrightness()
+const green = tinyColor('#00c060').getBrightness()
+const orange = tinyColor('#EF8745').getBrightness()
+const blue = tinyColor('#2090f0').getBrightness()
+console.log(red, green, orange, blue)
