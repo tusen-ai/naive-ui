d3fe50f8b0bf79817d19d497aa181a8dec5229c3
07akioni <07akioni2@gmail.com>
2019-10-15 16:25:53 +0800

    refactor(pagination): new algo to avoid tremble of page item

--- packages/common/Pagination/src/utils.js
+++ packages/common/Pagination/src/utils.js
@@ -5,6 +5,7 @@
  */
 function pagesToShow (currentPage, pageCount, pageSlot = 9) {
   if (pageCount === 1) return [1]
+  if (pageCount === 2) return [1, 2]
   const firstPage = 1
   const lastPage = pageCount
   let middleStart = currentPage
@@ -12,28 +13,28 @@ function pagesToShow (currentPage, pageCount, pageSlot = 9) {
   const middleDelta = (pageSlot - 5) / 2
   middleEnd += middleDelta
   middleEnd = Math.min(Math.max(middleEnd, firstPage + pageSlot - 3), lastPage - 2)
-  middleStart -= middleStart
+  middleStart -= middleDelta
   middleStart = Math.max(Math.min(middleStart, lastPage - pageSlot + 3), firstPage + 2)
   let leftSplit = false
   let rightSplit = false
-  if (middleStart > firstPage + 1) leftSplit = true
-  if (middleEnd < lastPage - 1) rightSplit = true
+  if (middleStart > firstPage + 2) leftSplit = true
+  if (middleEnd < lastPage - 2) rightSplit = true
   const items = []
   items.push(firstPage)
   if (leftSplit) {
     items.push(-2)
-  } else if (pageCount >= firstPage + 1) {
+  } else if (lastPage >= firstPage + 1) {
     items.push(firstPage + 1)
   }
-  for (let i = middleStart; i <= middleEnd && i < lastPage; ++i) {
+  for (let i = middleStart; i <= middleEnd; ++i) {
     items.push(i)
   }
   if (rightSplit) {
     items.push(-1)
-  } else if (middleEnd === lastPage - 2) {
+  } else if (middleEnd === lastPage - 2 && !(items[items.length - 1] === lastPage - 1)) {
     items.push(lastPage - 1)
   }
-  if (firstPage !== lastPage) items.push(lastPage)
+  if (!(items[items.length - 1] === lastPage)) items.push(lastPage)
   return items
 }
 
