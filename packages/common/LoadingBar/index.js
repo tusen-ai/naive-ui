a3b970ffe29c07316ff58c9923b9af5604b8b90c
07akioni <07akioni2@gmail.com>
2019-12-06 23:11:15 +0800

    chore: rename some files...

--- packages/common/LoadingBar/index.js
+++ packages/common/LoadingBar/index.js
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 import LoadingBar from './src/main.js'
-import { install } from '../../utils/installThemeableProperty'
+import { install } from '../../utils/installThemeAwarableProperty'
 
 LoadingBar.install = function (Vue) {
   install(Vue, LoadingBar, '$NLoadingBar')
