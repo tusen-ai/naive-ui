ffc943954d82f27281e7a32bcb95b926692244c8
07akioni <07akioni2@gmail.com>
2019-12-07 00:02:14 +0800

    feat(n-element)

---
+++ packages/common/Element/src/main.vue
@@ -0,0 +1,23 @@
+<script>
+import withapp from '../../../mixins/withapp'
+import themeable from '../../../mixins/themeable'
+import asthemecontext from '../../../mixins/asthemecontext'
+
+export default {
+  name: 'NElement',
+  mixins: [withapp, themeable, asthemecontext],
+  props: {
+    as: {
+      type: String,
+      default: 'div'
+    }
+  },
+  render (h) {
+    return h(this.as, {
+      class: {
+        [`n-${this.synthesizedTheme}-theme`]: this.synthesizedTheme
+      }
+    }, this.$slots.default || (this.$scopedSlots.default && this.$scopedSlots.default()) || null)
+  }
+}
+</script>
