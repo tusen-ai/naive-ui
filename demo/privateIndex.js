16eb131cbdd3feb36ead16d1a4dc4453e0a68cb9
07akioni <07akioni2@gmail.com>
2020-02-03 17:13:26 +0800

    chore: remove confirm.vue

--- demo/privateIndex.js
+++ demo/privateIndex.js
@@ -2,10 +2,10 @@
 import { Vue, router, i18n } from './init'
 import demoRouterView from './demoRouterView'
 
-if (localStorage.token === 'naive') {
-  new Vue({
-    ...demoRouterView,
-    i18n,
-    router
-  }).$mount('#app')
-}
+// if (localStorage.token === 'naive') {
+new Vue({
+  ...demoRouterView,
+  i18n,
+  router
+}).$mount('#app')
+// }
