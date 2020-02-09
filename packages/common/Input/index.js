69d4aa5d8d7301552abefc4eafde27296b0ae734
07akioni <07akioni2@gmail.com>
2020-02-09 14:14:32 +0800

    feat(input-group): input & select in input-group

--- packages/common/Input/index.js
+++ packages/common/Input/index.js
@@ -1,8 +1,10 @@
 /* istanbul ignore file */
-import Input from './src/main.vue'
+import Input from './src/Input.vue'
+import InputGroup from './src/InputGroup.vue'
 
 Input.install = function (Vue) {
   Vue.component(Input.name, Input)
+  Vue.component(InputGroup.name, InputGroup)
 }
 
 export default Input
---
+++ packages/common/Input/src/InputGroup.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="n-input-group">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NInputGroup'
+}
+</script>
