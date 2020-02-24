b5ff4fcdaeb1959a1fca0d2a4a25832bedfc8ec1
songwanli2025@163.com <songwanli2025@163.com>
2020-02-24 17:07:35 +0800

    merge

--- demo/loaders/NaiveUIDocLoader.js
+++ demo/loaders/NaiveUIDocLoader.js
@@ -1,6 +1,6 @@
 const convertMd2Doc = require('./convertMd2Doc')
 
 module.exports = function (content) {
-  let env = process.env.NODE_ENV
+  const env = process.env.NODE_ENV
   return convertMd2Doc(content, env)
 }
