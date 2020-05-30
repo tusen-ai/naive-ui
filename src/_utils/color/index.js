722e67356e71d310fca28bc6b33c620fb3f5e323
07akioni <07akioni2@gmail.com>
2020-05-30 16:39:33 +0800

    feat(utils/color): support more formats on read

--- src/_utils/color/index.js
+++ src/_utils/color/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-cond-assign */
 /**
  * The color utils are for internal usage, it needs to work first. API design
  * need to be refined later.
@@ -7,29 +8,77 @@ function floor (number) {
   return Math.floor(Number(number))
 }
 
+const prefix = '^\\s*'
+const suffix = '\\s*$'
+const num = '\\s*(\\d+)\\s*'
+const float = '\\s*((\\.\\d+)|(\\d+(\\.\\d*)?))\\s*'
+const hex = '([0-9A-Fa-f])'
+const dhex = '([0-9A-Fa-f]{2})'
+const rgbRegex = new RegExp(`${prefix}rgb\\s*\\(${num},${num},${num}\\)${suffix}`)
+const rgbaRegex = new RegExp(`${prefix}rgba\\s*\\(${num},${num},${num},${float}\\)${suffix}`)
+const sHexRegex = new RegExp(`${prefix}#${hex}${hex}${hex}${suffix}`)
+const hexRegex = new RegExp(`${prefix}#${dhex}${dhex}${dhex}${suffix}`)
+const sHexaRegex = new RegExp(`${prefix}#${hex}${hex}${hex}${hex}${suffix}`)
+const hexaRegex = new RegExp(`${prefix}#${dhex}${dhex}${dhex}${dhex}${suffix}`)
+
+function parseHex (value) {
+  return parseInt(value, 16)
+}
+
 /**
- *
- * @param {string} color support #000, #000000, rgb(0, 0, 0)
- * @returns [r, g, b]
+ * @param {string} color support #000[0], #000000[00], rgb(0, 0, 0), rgba(0, 0, 0, 0)
+ * @returns [r, g, b, a]
  */
 export function read (color) {
-  if (!color) return null
-  if (color.startsWith('#')) {
-    let channels = null
-    if (color.length === 4) channels = color.slice(1, 4).split('').map(c => c + c)
-    else if (color.length === 7) {
-      channels = [
-        color.slice(1, 3),
-        color.slice(3, 5),
-        color.slice(5, 7)
+  try {
+    let i
+    if (i = hexRegex.exec(color)) {
+      return [
+        parseHex(i[1]),
+        parseHex(i[2]),
+        parseHex(i[3]),
+        1
       ]
-    } else throw new Error('Invalid color value: ' + color)
-    return channels.map(c => parseInt(c, 16))
+    } else if (i = rgbRegex.exec(color)) {
+      return [
+        parseInt(i[1]),
+        parseInt(i[2]),
+        parseInt(i[3]),
+        1
+      ]
+    } else if (i = rgbaRegex.exec(color)) {
+      return [
+        parseInt(i[1]),
+        parseInt(i[2]),
+        parseInt(i[3]),
+        Number(i[4])
+      ]
+    } else if (i = sHexRegex.exec(color)) {
+      return [
+        parseHex(i[1] + i[1]),
+        parseHex(i[2] + i[2]),
+        parseHex(i[3] + i[3]),
+        1
+      ]
+    } else if (i = hexaRegex.exec(color)) {
+      return [
+        parseHex(i[1]),
+        parseHex(i[2]),
+        parseHex(i[3]),
+        parseHex(i[4]) / 255
+      ]
+    } else if (i = sHexaRegex.exec(color)) {
+      return [
+        parseHex(i[1] + i[1]),
+        parseHex(i[2] + i[2]),
+        parseHex(i[3] + i[3]),
+        parseHex(i[4] + i[4]) / 255
+      ]
+    }
+    throw new Error('[naive-ui/utils/color]: Invalid color value ' + color)
+  } catch (e) {
+    console.log(e)
   }
-  const rgbRegex = /rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)/
-  const rgbResult = color.match(rgbRegex)
-  if (rgbResult) return [rgbResult[1], rgbResult[2], rgbResult[3]]
-  throw new Error('Invalid color value: ' + color)
 }
 
 /**
@@ -38,7 +87,7 @@ export function read (color) {
  * @param {[number, number, number, number]} overlay
  */
 export function composite (base, overlay) {
-  return 'rgb(' + base.map((v, i) => floor(v * (1 - overlay[3]) + overlay[i] * overlay[3])).join(', ') + ')'
+  return 'rgb(' + base.slice(0, 3).map((v, i) => floor(v * (1 - overlay[3]) + overlay[i] * overlay[3])).join(', ') + ')'
 }
 
 export function changeColor (base, options) {
