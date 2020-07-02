1f4a283fd60d77ca7ebd8220da10d8b3bace6d79
07akioni <07akioni2@gmail.com>
2020-07-03 00:33:20 +0800

    refactor(input-group): use cssr

--- src/input-group/src/InputGroup.vue
+++ src/input-group/src/InputGroup.vue
@@ -5,7 +5,13 @@
 </template>
 
 <script>
+import usecssr from '../../_mixins/usecssr'
+import styles from './styles'
+
 export default {
-  name: 'NInputGroup'
+  name: 'NInputGroup',
+  mixins: [
+    usecssr(styles)
+  ]
 }
 </script>
