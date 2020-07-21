32c589a61798ecf2916285b0a73169c2354472d1
07akioni <07akioni2@gmail.com>
2020-07-22 00:25:36 +0800

    chore: create-cssr-scaffold script

---
+++ src/alert/src/styles/themed-base.cssr.js
@@ -0,0 +1,6 @@
+import { c, cTB, cB, cE, cM, cNotM } from '../../../_utils/cssr'
+
+export default c([
+  ({ props }) => {
+  }
+])
---
+++ src/alert/styles/light.js
@@ -0,0 +1,9 @@
+import create from '../../styles/_utils/create-component-base'
+
+export default create({
+  theme: 'light',
+  name: '',
+  getDerivedVariables ({ base, derived }) {
+    return {}
+  }
+})
