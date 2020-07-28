6a2ed5df0032bdb7076c4e8511d20fc079855ac5
07akioni <07akioni2@gmail.com>
2020-07-28 20:21:22 +0800

    chore: remove upper case avatar folder (#171)
    * Delete index.js
    
    * Delete themed-base.cssr.js
    
    * Delete dark.js
    
    * Delete light.js

--- src/Avatar/styles/light.js
+++
@@ -1,9 +0,0 @@
-import create from '../../styles/_utils/create-component-base'
-
-export default create({
-  theme: 'light',
-  name: '',
-  getDerivedVariables ({ base, derived }) {
-    return {}
-  }
-})
