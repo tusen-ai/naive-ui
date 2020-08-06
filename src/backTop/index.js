83cea6c2bbf5ef422fa43bf512467d0a231190f4
07akioni <07akioni2@gmail.com>
2020-08-06 22:07:48 +0800

    chore

--- src/backTop/index.js
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-import BackTop from './src/BackTop.vue'
-
-BackTop.install = function (Vue, naive) {
-  Vue.component(naive.componentPrefix + BackTop.name, BackTop)
-}
-
-export default BackTop
