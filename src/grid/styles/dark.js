7973020ed6a14e49ba066631af3bbb6b01da5fba
07akioni <07akioni2@gmail.com>
2020-08-27 21:18:44 +0800

    refactor(grid): clean cssr codes (#283)

---
+++ src/grid/styles/dark.js
@@ -0,0 +1,9 @@
+import create from '../../styles/_utils/create-component-base'
+
+export default create({
+  theme: 'dark',
+  name: 'Grid',
+  getDerivedVariables ({ base, derived }) {
+    return {}
+  }
+})
