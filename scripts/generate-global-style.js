6022181a8988c5ec3c61669d138e06cfeef59c6c
07akioni <07akioni2@gmail.com>
2020-09-08 13:52:03 +0800

    refactor: extract global style to cssr

---
+++ scripts/generate-global-style.js
@@ -0,0 +1,27 @@
+const fs = require('fs').promises
+const path = require('path')
+
+function generateCssr (literal) {
+  return `export default \`${literal}\`\n`
+}
+
+;(async () => {
+  const file = await fs.readFile(
+    path.resolve(
+      __dirname,
+      '..',
+      'node_modules',
+      'vue-virtual-scroller',
+      'dist',
+      'vue-virtual-scroller.css'
+    )
+  )
+  const cssLiteral = file.toString()
+  await fs.writeFile(path.resolve(
+    __dirname,
+    '..',
+    'src',
+    '_global-style/',
+    'vue-virtual-scroller.cssr.js'
+  ), generateCssr(cssLiteral))
+})()
---
+++ src/_global-style/index.cssr.js
@@ -0,0 +1,35 @@
+import { c, cB } from '../_utils/cssr'
+import virtualScrollerStyle from './vue-virtual-scroller.cssr'
+import commonVariables from '../styles/_common-style/base'
+
+export default c([
+  virtualScrollerStyle,
+  c('body', {
+    margin: 0,
+    fontSize: '14px',
+    fontFamily: commonVariables.fontFamily
+  }, [
+    c('>', [
+      cB('positioning-container', {
+        position: 'fixed'
+      })
+    ])
+  ]),
+  c('input', {
+    fontSize: 'inherit',
+    fontFamily: commonVariables.fontFamily
+  }),
+  cB('positioning-container', {
+    raw: `
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      pointer-events: none;
+      transform: translateZ(0);
+    `
+  }, cB('positioning-content', {
+    pointerEvents: 'all'
+  }))
+])
