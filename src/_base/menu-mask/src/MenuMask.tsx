25b5f486c631d1943558fffdac989cf27d38bf14
07akioni <07akioni2@gmail.com>
2021-01-18 14:29:23 +0800

    refactor(base-menu-mask): ts

---
+++ src/_base/menu-mask/src/MenuMask.tsx
@@ -0,0 +1,43 @@
+import { h, ref, onBeforeUnmount, defineComponent, Transition } from 'vue'
+import { useStyle } from '../../../_mixins'
+import style from './styles/index.cssr'
+
+export default defineComponent({
+  name: 'BaseMenuMask',
+  setup () {
+    useStyle('BaseMenuMask', style)
+    const messageRef = ref<string | null>(null)
+    let timerId: number | null = null
+    const uncontrolledShowRef = ref(false)
+    onBeforeUnmount(() => {
+      if (timerId !== null) {
+        window.clearTimeout(timerId)
+      }
+    })
+    return {
+      message: messageRef,
+      show: uncontrolledShowRef,
+      showOnce (message: string, duration = 1500) {
+        if (timerId) window.clearTimeout(timerId)
+        uncontrolledShowRef.value = true
+        messageRef.value = message
+        timerId = window.setTimeout(() => {
+          uncontrolledShowRef.value = false
+          messageRef.value = null
+        }, duration)
+      }
+    }
+  },
+  render () {
+    return (
+      <Transition name="n-fade-in-transition">
+        {{
+          default: () =>
+            this.show ? (
+              <div class="n-base-menu-mask">{this.message}</div>
+            ) : null
+        }}
+      </Transition>
+    )
+  }
+})
