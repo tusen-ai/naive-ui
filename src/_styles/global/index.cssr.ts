1fa211188190a336172f99f54bf416633a0f0abf
07akioni <07akioni2@gmail.com>
2021-01-27 10:56:25 +0800

    fix(global-style): input el font size & family

--- src/_styles/global/index.cssr.ts
+++ src/_styles/global/index.cssr.ts
@@ -5,4 +5,9 @@ export default c('body', {
   margin: 0,
   fontSize: commonVariables.fontSize,
   fontFamily: commonVariables.fontFamily
-})
+}, [
+  c('input', {
+    fontFamily: 'inherit',
+    fontSize: 'inherit'
+  })
+])
