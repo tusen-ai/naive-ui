d94e7f8a6a7f24a0c4af752a52ae59490d31a1fb
07akioni <07akioni2@gmail.com>
2021-02-05 16:38:01 +0800

    site: add highlight html/xml lang

--- demo/store/hljs.js
+++ demo/store/hljs.js
@@ -2,10 +2,12 @@ import hljs from 'highlight.js/lib/core'
 import javascript from 'highlight.js/lib/languages/javascript'
 import python from 'highlight.js/lib/languages/python'
 import cpp from 'highlight.js/lib/languages/cpp'
+import xml from 'highlight.js/lib/languages/xml'
 
 hljs.registerLanguage('javascript', javascript)
 hljs.registerLanguage('python', python)
 hljs.registerLanguage('cpp', cpp)
+hljs.registerLanguage('html', xml)
 hljs.registerLanguage('naive-log', () => ({
   contains: [
     {
