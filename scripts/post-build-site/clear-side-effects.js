a4e6d5c0e62d0906848648cdffe94c0f4a88c8a9
07akioni <07akioni2@gmail.com>
2021-02-05 15:06:13 +0800

    fix: build site

--- scripts/post-build-site/clear-side-effects.js
+++ scripts/post-build-site/clear-side-effects.js
@@ -3,6 +3,6 @@ const packageJson = require('../../package.json')
 packageJson.sideEffects = false
 
 require('fs-extra').writeFileSync(
-  require('path').resolve(__dirname, '..', 'package.json'),
+  require('path').resolve(__dirname, '..', '..', 'package.json'),
   JSON.stringify(packageJson, 0, 2) + '\n'
 )
--- scripts/pre-build-site/restore-side-effects.js
+++ scripts/pre-build-site/restore-side-effects.js
@@ -3,6 +3,6 @@ const packageJson = require('../../package.json')
 delete packageJson.sideEffects
 
 require('fs-extra').writeFileSync(
-  require('path').resolve(__dirname, '..', 'package.json'),
+  require('path').resolve(__dirname, '..', '..', 'package.json'),
   JSON.stringify(packageJson, 0, 2) + '\n'
 )
