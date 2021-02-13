2162038f5ca807f785ddfc362e50612f271279c4
07akioni <07akioni2@gmail.com>
2021-02-13 21:38:12 +0800

    fix(message): default icon margin

--- src/message/styles/_common.ts
+++ src/message/styles/_common.ts
@@ -3,7 +3,7 @@ export default {
   padding: '10px 20px',
   maxWidth: '720px',
   minWidth: '420px',
-  iconMargin: '10px',
+  iconMargin: '0 10px 0 0',
   closeMargin: '0 0 0 12px',
   closeSize: '16px',
   iconSize: '20px',
