25f72e39ab933f6651a16b4ed23f4c737c1bf71b
07akioni <07akioni2@gmail.com>
2021-02-17 15:28:20 +0800

    test(time)

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.(ts|tsx)$'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.(ts|tsx)'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
---
+++ src/time/tests/Time.spec.ts
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils'
+import { NTime } from '../index'
+
+describe('n-time', () => {
+  it('should work with import on demand', () => {
+    mount(NTime)
+  })
+})
