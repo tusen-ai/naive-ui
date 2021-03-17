7068e5b35f95ce91a2706d069e626202f1e39da0
07akioni <07akioni2@gmail.com>
2021-03-17 18:14:54 +0800

    refactor: use type directly from drawer in use-adjusted-to

--- src/_utils/composable/use-adjusted-to.ts
+++ src/_utils/composable/use-adjusted-to.ts
@@ -1,21 +1,18 @@
 import { useMemo } from 'vooks'
 import { ComponentPublicInstance, ComputedRef, inject } from 'vue'
 import type { ModalBodyInjection } from '../../modal/src/BodyWrapper'
+import type { DrawerBodyInjection } from '../../drawer/src/DrawerBodyWrapper'
 
 interface UseAdjustedToProps {
   to?: string | HTMLElement
   [key: string]: unknown
 }
 
-interface DrawerInjection {
-  bodyRef: HTMLElement
-}
-
 export function useAdjustedTo (
   props: UseAdjustedToProps
 ): ComputedRef<HTMLElement | string> {
   const modal = inject<ModalBodyInjection | null>('NModalBody', null)
-  const drawer = inject<DrawerInjection | null>('NDrawerBody', null)
+  const drawer = inject<DrawerBodyInjection | null>('NDrawerBody', null)
   return useMemo(() => {
     const { to } = props
     if (to !== undefined) return to
