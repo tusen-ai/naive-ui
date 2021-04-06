5491320f9eeed6d01f4125e65dd5735175e007d3
07akioni <07akioni2@gmail.com>
2021-04-06 17:45:42 +0800

    fix: add -webkit-text-size-adjust to global style
    IOS safari will have error font size if not set.

--- src/_styles/global/index.cssr.ts
+++ src/_styles/global/index.cssr.ts
@@ -1,13 +1,20 @@
 import { c } from '../../_utils/cssr'
 import commonVariables from '../common/_common'
 
-export default c('body', {
-  margin: 0,
-  fontSize: commonVariables.fontSize,
-  fontFamily: commonVariables.fontFamily
-}, [
-  c('input', {
-    fontFamily: 'inherit',
-    fontSize: 'inherit'
-  })
+// All the components need the style
+// It is static and won't be changed in the app's lifetime
+// If user want to overrides it `n-global-style` is provided
+//
+// Technically we can remove font-size & font-family to make
+// it pure. I'm not sure whether to do it.
+export default c('body', `
+  margin: 0;
+  font-size: ${commonVariables.fontSize};
+  font-family: ${commonVariables.fontFamily};
+  -webkit-text-size-adjust: 100%;
+`, [
+  c('input', `
+    font-family: inherit;
+    font-size: inherit;
+  `)
 ])
