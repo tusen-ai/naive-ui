8e356947b008d56bd0743101043a16f7783c259b
07akioni <07akioni2@gmail.com>
2021-04-16 15:45:14 +0800

    feat(divider): clsPrefix

--- src/divider/index.ts
+++ src/divider/index.ts
@@ -1,2 +1,2 @@
-/* istanbul ignore file */
 export { default as NDivider } from './src/Divider'
+export type { DividerProps } from './src/Divider'
--- src/divider/src/Divider.tsx
+++ src/divider/src/Divider.tsx
@@ -8,31 +8,44 @@ import {
 } from 'vue'
 import { useConfig, useTheme } from '../../_mixins'
 import type { ThemeProps } from '../../_mixins'
+import type { ExtractPublicPropTypes } from '../../_utils'
 import { dividerLight } from '../styles'
 import type { DividerTheme } from '../styles'
 import style from './styles/index.cssr'
 
+const dividerProps = {
+  ...(useTheme.props as ThemeProps<DividerTheme>),
+  titlePlacement: {
+    type: String as PropType<'left' | 'center' | 'right'>,
+    default: 'center'
+  },
+  dashed: {
+    type: Boolean,
+    default: false
+  },
+  vertical: {
+    type: Boolean,
+    default: false
+  }
+} as const
+
+export type DividerProps = ExtractPublicPropTypes<typeof dividerProps>
+
 export default defineComponent({
   name: 'Divider',
-  props: {
-    ...(useTheme.props as ThemeProps<DividerTheme>),
-    titlePlacement: {
-      type: String as PropType<'left' | 'center' | 'right'>,
-      default: 'center'
-    },
-    dashed: {
-      type: Boolean,
-      default: false
-    },
-    vertical: {
-      type: Boolean,
-      default: false
-    }
-  },
+  props: dividerProps,
   setup (props) {
-    const themeRef = useTheme('Divider', 'Divider', style, dividerLight, props)
+    const { mergedClsPrefix } = useConfig(props)
+    const themeRef = useTheme(
+      'Divider',
+      'Divider',
+      style,
+      dividerLight,
+      props,
+      mergedClsPrefix
+    )
     return {
-      ...useConfig(props),
+      cPrefix: mergedClsPrefix,
       cssVars: computed(() => {
         const {
           common: { cubicBezierEaseInOut },
@@ -48,28 +61,32 @@ export default defineComponent({
     }
   },
   render () {
-    const { $slots, titlePlacement, vertical, dashed, cssVars } = this
+    const { $slots, titlePlacement, vertical, dashed, cssVars, cPrefix } = this
     return (
       <div
         class={[
-          'n-divider',
+          `${cPrefix}-divider`,
           {
-            'n-divider--vertical': vertical,
-            'n-divider--no-title': !$slots.default,
-            'n-divider--dashed': dashed,
-            [`n-divider--title-position-${titlePlacement}`]:
+            [`${cPrefix}-divider--vertical`]: vertical,
+            [`${cPrefix}-divider--no-title`]: !$slots.default,
+            [`${cPrefix}-divider--dashed`]: dashed,
+            [`${cPrefix}-divider--title-position-${titlePlacement}`]:
               $slots.default && titlePlacement
           }
         ]}
         style={cssVars as CSSProperties}
       >
         {!vertical ? (
-          <hr class="n-divider__line n-divider__line--left" />
+          <hr
+            class={`${cPrefix}-divider__line ${cPrefix}-divider__line--left`}
+          />
         ) : null}
         {!vertical && $slots.default ? (
           <>
-            <div class="n-divider__title">{this.$slots}</div>
-            <div class="n-divider__line n-divider__line--right" />
+            <div class={`${cPrefix}-divider__title`}>{this.$slots}</div>
+            <div
+              class={`${cPrefix}-divider__line ${cPrefix}-divider__line--right`}
+            />
           </>
         ) : null}
       </div>
