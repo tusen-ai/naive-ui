06e052150f91c6030d678704ac59f88016e6e197
07akioni <07akioni2@gmail.com>
2021-06-05 21:27:57 +0800

    test(collapse): arrow slot

--- src/collapse/tests/Collapse.spec.ts
+++
@@ -1,8 +0,0 @@
-import { mount } from '@vue/test-utils'
-import { NCollapse } from '../index'
-
-describe('n-collapse', () => {
-  it('should work with import on demand', () => {
-    mount(NCollapse)
-  })
-})
---
+++ src/collapse/tests/Collapse.spec.tsx
@@ -0,0 +1,23 @@
+import { h } from 'vue'
+import { mount } from '@vue/test-utils'
+import { NCollapse, NCollapseItem } from '../index'
+
+describe('n-collapse', () => {
+  it('should work with import on demand', () => {
+    mount(NCollapse)
+  })
+  it('can customize icon', () => {
+    const wrapper = mount(() => {
+      return (
+        <NCollapse>
+          {{
+            arrow: () => <div class="my-icon"></div>,
+            default: () => <NCollapseItem name="1"></NCollapseItem>
+          }}
+        </NCollapse>
+      )
+    })
+    console.log(wrapper.html())
+    expect(wrapper.find('.my-icon').exists()).toEqual(true)
+  })
+})
