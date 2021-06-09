dc77bb353c83922207b25db69145f86e141802d2
陈轶男 <chenyinan@moqipobing.com>
2021-06-09 11:13:12 +0800

    feat(card): rewrite target style

--- src/card/src/styles/index.cssr.ts
+++ src/card/src/styles/index.cssr.ts
@@ -137,8 +137,9 @@ export default c([
         width: 100%;
       `)
     ]),
-    cM('bordered', [
-      'border: 1px solid var(--border-color)',
+    cM('bordered', `
+      border: 1px solid var(--border-color);
+    `, [
       c('&:target', 'border-color: var(--color-target);')
     ]),
     cM('action-segmented', [
