1dffb063b44b674f2c0ab18bb8df83ec0eb60102
ahwgs <ah_wgs@126.com>
2021-06-10 12:39:08 +0800

    test(dropdown): add disabled case

--- src/dropdown/tests/Dropdown.spec.ts
+++ src/dropdown/tests/Dropdown.spec.ts
@@ -9,4 +9,44 @@ describe('n-dropdown', () => {
       }
     })
   })
+
+  it('dropdown disabled', async () => {
+    const onSelect = (key: any): void => {
+      expect(key).toEqual(undefined)
+    }
+
+    const options = [
+      {
+        label: '滨海湾金沙，新加坡',
+        key: 'marina bay sands',
+        disabled: true
+      }
+    ]
+    const triggerEvent = 'click'
+    const wrapper = mount(NDropdown, {
+      attachTo: document.body,
+      props: {
+        options,
+        trigger: triggerEvent,
+        onSelect: onSelect
+      },
+      slots: {
+        default: () => 'star kirby'
+      }
+    })
+
+    const triggerNodeWrapper = wrapper.find('span')
+    expect(triggerNodeWrapper.exists()).toBe(true)
+    await triggerNodeWrapper.trigger(triggerEvent)
+
+    const disabledMenu = document.querySelector(
+      '.n-dropdown-option-body--disabled'
+    ) as HTMLDivElement
+
+    expect(disabledMenu).not.toEqual(null)
+
+    await disabledMenu.click()
+
+    wrapper.unmount()
+  })
 })
