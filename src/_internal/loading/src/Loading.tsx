c65b068a52f80fe8b4f88bfa0b6d0cbfbef1c214
07akioni <07akioni2@gmail.com>
2021-06-11 00:37:59 +0800

    fix(spin): animation shifts (#66)
    * fix(spin): animation shifts
    
    * docs

--- src/_internal/loading/src/Loading.tsx
+++ src/_internal/loading/src/Loading.tsx
@@ -3,6 +3,10 @@ import { useStyle } from '../../../_mixins'
 import NIconSwitchTransition from '../../icon-switch-transition'
 import style from './styles/index.cssr'
 
+const duration = '1.6s'
+
+// The loading svg dom comes from https://codepen.io/FezVrasta/pen/oXrgdR
+
 export default defineComponent({
   name: 'BaseLoading',
   props: {
@@ -35,33 +39,62 @@ export default defineComponent({
     useStyle('BaseLoading', style, toRef(props, 'clsPrefix'))
   },
   render () {
-    const { clsPrefix } = this
+    const { clsPrefix, radius, strokeWidth, stroke, scale } = this
+    const scaledRadius = radius / scale
     return (
-      <div
-        class={`${clsPrefix}-base-loading`}
-        style={{ stroke: this.stroke }}
-        role="img"
-        aria-label="loading"
-      >
+      <div class={`${clsPrefix}-base-loading`} role="img" aria-label="loading">
         <NIconSwitchTransition>
           {{
             default: () =>
               this.show ? (
                 <svg
-                  key="loading"
-                  class={`${clsPrefix}-base-loading-circular ${clsPrefix}-base-loading__icon`}
-                  viewBox={`0 0 ${(this.radius * 2) / this.scale} ${
-                    (this.radius * 2) / this.scale
-                  }`}
+                  class={`${clsPrefix}-base-loading__icon`}
+                  viewBox={`0 0 ${2 * scaledRadius} ${2 * scaledRadius}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{ color: stroke }}
                 >
-                  <circle
-                    style={{ strokeWidth: this.strokeWidth }}
-                    class={`${clsPrefix}-base-loading-circular-path`}
-                    cx={this.radius / this.scale}
-                    cy={this.radius / this.scale}
-                    fill="none"
-                    r={this.radius - this.strokeWidth / 2}
-                  />
+                  <g>
+                    <animateTransform
+                      attributeName="transform"
+                      type="rotate"
+                      values={`0 ${scaledRadius} ${scaledRadius};270 ${scaledRadius} ${scaledRadius}`}
+                      begin="0s"
+                      dur={duration}
+                      fill="freeze"
+                      repeatCount="indefinite"
+                    />
+                    <circle
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width={strokeWidth}
+                      stroke-linecap="round"
+                      cx={scaledRadius}
+                      cy={scaledRadius}
+                      r={radius - strokeWidth / 2}
+                      stroke-dasharray={5.67 * radius}
+                      stroke-dashoffset={18.48 * radius}
+                    >
+                      <animateTransform
+                        attributeName="transform"
+                        type="rotate"
+                        values={`0 ${scaledRadius} ${scaledRadius};135 ${scaledRadius} ${scaledRadius};450 ${scaledRadius} ${scaledRadius}`}
+                        begin="0s"
+                        dur={duration}
+                        fill="freeze"
+                        repeatCount="indefinite"
+                      />
+                      <animate
+                        attributeName="stroke-dashoffset"
+                        values={`${5.67 * radius};${1.42 * radius};${
+                          5.67 * radius
+                        }`}
+                        begin="0s"
+                        dur={duration}
+                        fill="freeze"
+                        repeatCount="indefinite"
+                      />
+                    </circle>
+                  </g>
                 </svg>
               ) : (
                 <div
