851481925537e3d7201ac0676afd928544245c0d
07akioni <07akioni2@gmail.com>
2021-06-11 12:00:11 +0800

    fix(menu): lack `on-update-value` and `on-update-expanded-keys` props

--- src/menu/tests/Menu.spec.ts
+++ src/menu/tests/Menu.spec.ts
@@ -5,4 +5,30 @@ describe('n-menu', () => {
   it('should work with import on demand', () => {
     mount(NMenu)
   })
+  it('props.onUpdateValue type', () => {
+    const stringCb = (v: string): void => {}
+    const numberCb = (v: number): void => {}
+    const snCb = (v: string | number): void => {}
+    const stringArrCb = (v: string[]): void => {}
+    const numberArrCb = (v: number[]): void => {}
+    const snArrCb = (v: Array<string | number>): void => {}
+    mount(NMenu, {
+      props: {
+        onUpdateValue: stringCb,
+        onUpdateExpandedKeys: stringArrCb
+      }
+    })
+    mount(NMenu, {
+      props: {
+        onUpdateValue: numberCb,
+        onUpdateExpandedKeys: numberArrCb
+      }
+    })
+    mount(NMenu, {
+      props: {
+        onUpdateValue: snCb,
+        onUpdateExpandedKeys: snArrCb
+      }
+    })
+  })
 })
