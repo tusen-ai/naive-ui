b18b742055b7a795e0e43c5422630ec5ef934b13
ahwgs <ah_wgs@126.com>
2021-06-15 23:49:53 +0800

    test(space): add space test (#138)
    * test(space): add space test
    
    * test(space): update snapshot

--- src/space/tests/Space.spec.ts
+++
@@ -1,8 +0,0 @@
-import { mount } from '@vue/test-utils'
-import { NSpace } from '../index'
-
-describe('n-space', () => {
-  it('should work with import on demand', () => {
-    mount(NSpace)
-  })
-})
