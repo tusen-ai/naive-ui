db42e006b7838ebc67151e4088b0fbaa40fc5bbb
xiezongchen <952937570@qq.com>
2021-06-21 21:07:19 +0800

    feat: optimization

--- src/button/tests/Button.spec.tsx
+++ src/button/tests/Button.spec.tsx
@@ -64,24 +64,16 @@ describe('n-button', () => {
     const wrapper = mount(NButton)
 
     await wrapper.setProps({ size: 'tiny' })
-    expect(wrapper.find('button').attributes('style')).toContain(
-      '--height: 22px; --font-size: 12px; --padding: 0 6px; --icon-size: 14px;'
-    )
+    expect(wrapper.find('button').attributes('style')).toMatchSnapshot()
 
     await wrapper.setProps({ size: 'small' })
-    expect(wrapper.find('button').attributes('style')).toContain(
-      '--height: 28px; --font-size: 14px; --padding: 0 10px; --icon-size: 18px;'
-    )
+    expect(wrapper.find('button').attributes('style')).toMatchSnapshot()
 
     await wrapper.setProps({ size: 'medium' })
-    expect(wrapper.find('button').attributes('style')).toContain(
-      '--height: 34px; --font-size: 14px; --padding: 0 14px; --icon-size: 18px;'
-    )
+    expect(wrapper.find('button').attributes('style')).toMatchSnapshot()
 
     await wrapper.setProps({ size: 'large' })
-    expect(wrapper.find('button').attributes('style')).toContain(
-      '--height: 40px; --font-size: 15px; --padding: 0 18px; --icon-size: 20px;'
-    )
+    expect(wrapper.find('button').attributes('style')).toMatchSnapshot()
   })
 
   it('should work with `text` prop', () => {
@@ -93,9 +85,7 @@ describe('n-button', () => {
         default: () => 'test'
       }
     })
-    expect(wrapper.find('button').attributes('style')).toContain(
-      '--border: none; --border-hover: none; --border-pressed: none; --border-focus: none; --border-disabled: none; --width: initial; --height: initial; --font-size: 14px; --padding: initial;'
-    )
+    expect(wrapper.find('button').attributes('style')).toMatchSnapshot()
     expect(wrapper.find('.n-button__content').element.textContent).toBe('test')
   })
 
@@ -113,9 +103,7 @@ describe('n-button', () => {
       }
     })
 
-    expect(wrapper.find('a').attributes('style')).toContain(
-      '--border: none; --border-hover: none; --border-pressed: none; --border-focus: none; --border-disabled: none; --width: initial; --height: initial; --font-size: 14px; --padding: initial;'
-    )
+    expect(wrapper.find('a').attributes('style')).toMatchSnapshot()
     expect(wrapper.find('a').classes()).toContain('n-button--primary-type')
     expect(wrapper.find('a').attributes('type')).toContain('button')
     expect(wrapper.find('a').attributes('disabled')).toContain('false')
