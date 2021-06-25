7ce6b92e889ac4227613119fc3f2fee96226945c
XieZongChen <46394163+amadeus711@users.noreply.github.com>
2021-06-25 10:39:15 +0800

    test(card): add card test (#290)
    * test(card): add card test
    
    * test(card): optimization

--- src/card/tests/Card.spec.ts
+++ src/card/tests/Card.spec.ts
@@ -5,4 +5,135 @@ describe('n-card', () => {
   it('should work with import on demand', () => {
     mount(NCard)
   })
+
+  it('should work with `title` prop', async () => {
+    const wrapper = mount(NCard)
+
+    expect(wrapper.find('.n-card-header').exists()).toBe(false)
+
+    await wrapper.setProps({ title: 'test' })
+    expect(wrapper.find('.n-card-header').exists()).toBe(true)
+    expect(wrapper.find('.n-card-header__main').text()).toBe('test')
+  })
+
+  it('should work with `size` prop', async () => {
+    const wrapper = mount(NCard)
+
+    await wrapper.setProps({ size: 'small' })
+    expect(wrapper.find('.n-card').attributes('style')).toMatchSnapshot()
+  })
+
+  it('should work with `hoverable` prop', async () => {
+    const wrapper = mount(NCard, {
+      props: {
+        hoverable: true
+      }
+    })
+
+    expect(wrapper.find('.n-card').classes()).toContain('n-card--hoverable')
+  })
+
+  it('should work with `slots` ', async () => {
+    const wrapper = mount(NCard, {
+      slots: {
+        cover: () => 'cover',
+        header: () => 'header',
+        'header-extra': () => 'header-extra',
+        default: () => 'content',
+        footer: () => 'footer',
+        action: () => 'action'
+      }
+    })
+
+    expect(wrapper.find('.n-card-cover').exists()).toBe(true)
+    expect(wrapper.find('.n-card-cover').text()).toBe('cover')
+
+    expect(wrapper.find('.n-card-header').exists()).toBe(true)
+    expect(wrapper.find('.n-card-header__main').text()).toBe('header')
+
+    expect(wrapper.find('.n-card-header__extra').exists()).toBe(true)
+    expect(wrapper.find('.n-card-header__extra').text()).toBe('header-extra')
+
+    expect(wrapper.find('.n-card__content').exists()).toBe(true)
+    expect(wrapper.find('.n-card__content').text()).toBe('content')
+
+    expect(wrapper.find('.n-card__footer').exists()).toBe(true)
+    expect(wrapper.find('.n-card__footer').text()).toBe('footer')
+
+    expect(wrapper.find('.n-card__action').exists()).toBe(true)
+    expect(wrapper.find('.n-card__action').text()).toBe('action')
+  })
+
+  it('should work with `bordered` prop', async () => {
+    const wrapper = mount(NCard, {
+      props: {
+        bordered: false
+      }
+    })
+
+    expect(wrapper.find('.n-card--bordered').exists()).toBe(false)
+  })
+
+  it('should work with `closable` and `on-close` prop', async () => {
+    const onClose = jest.fn()
+    const wrapper = mount(NCard, {
+      props: {
+        closable: true,
+        onClose
+      }
+    })
+
+    expect(wrapper.find('.n-card-header__close').exists()).toBe(true)
+    await wrapper.find('.n-card-header__close').trigger('click')
+    expect(onClose).toHaveBeenCalled()
+  })
+
+  it('should work with `header-style` prop', async () => {
+    const testStyle = 'padding: 0px;'
+    const wrapper = mount(NCard, {
+      props: {
+        title: 'test',
+        headerStyle: testStyle
+      },
+      slots: {
+        default: () => 'test'
+      }
+    })
+
+    expect(wrapper.find('.n-card-header').attributes('style')).toContain(
+      testStyle
+    )
+  })
+
+  it('should work with `content-style` prop', async () => {
+    const testStyle = 'padding: 0px;'
+    const wrapper = mount(NCard, {
+      props: {
+        contentStyle: testStyle
+      },
+      slots: {
+        default: () => 'test'
+      }
+    })
+
+    expect(wrapper.find('.n-card__content').attributes('style')).toContain(
+      testStyle
+    )
+  })
+
+  it('should work with `footer-style` prop', async () => {
+    const testStyle = 'padding: 0px;'
+    const wrapper = mount(NCard, {
+      props: {
+        footerStyle: testStyle
+      },
+      slots: {
+        footer: () => 'test'
+      }
+    })
+
+    expect(wrapper.find('.n-card__footer').attributes('style')).toContain(
+      testStyle
+    )
+  })
 })
