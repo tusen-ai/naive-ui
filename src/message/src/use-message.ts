16bad549966a3274cf580e406874707ebcdee65d
Volankey <935196116@qq.com>
2021-06-27 13:35:23 +0800

    feat:(message): add useMessage usage link of prerequisite

--- src/message/src/use-message.ts
+++ src/message/src/use-message.ts
@@ -6,7 +6,10 @@ import type { MessageApiInjection } from './MessageProvider'
 export function useMessage (): MessageApiInjection {
   const api = inject(messageApiInjectionKey, null)
   if (api === null) {
-    throwError('use-message', 'No outer <n-message-provider /> founded.')
+    throwError(
+      'use-message',
+      'No outer <n-message-provider /> founded. See Prerequisite in https://www.naiveui.com/en-US/os-theme/components/message for more details.'
+    )
   }
   return api
 }
