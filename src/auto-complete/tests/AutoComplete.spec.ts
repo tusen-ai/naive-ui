9aade08084060bbe41b74228184bcc88da2445b4
wanli.song@tusimple.ai <wanli.song@tusimple.ai>
2021-06-28 13:35:38 +0800

    test(auto-complete): add test

--- src/auto-complete/tests/AutoComplete.spec.ts
+++ src/auto-complete/tests/AutoComplete.spec.ts
@@ -1,8 +1,32 @@
 import { mount } from '@vue/test-utils'
-import { NAutoComplete } from '../index'
+import { NAutoComplete, AutoCompleteProps } from '../index'
 
 describe('n-auto-complete', () => {
   it('should work with import on demand', () => {
     mount(NAutoComplete)
   })
 })
+
+it('should work with `loading` prop', async () => {
+  const options: AutoCompleteProps['options'] = ['@gmail.com', '@163.com', '@qq.com'].map((suffix) => {
+    const prefix = 'test'
+    return {
+      label: prefix + suffix,
+      value: prefix + suffix
+    }
+  })
+  const wrapper = mount(NAutoComplete, {
+    props: {
+      loading: true,
+      value: 'test',
+      options: options
+    }
+  })
+  const triggerNodeWrapper = wrapper.find('input')
+  expect(triggerNodeWrapper.exists()).toBe(true)
+  await triggerNodeWrapper.trigger('focus')
+  expect(document.querySelector('.n-base-select-menu__loading')).not.toEqual(null)
+  wrapper.unmount()
+})
+
+
