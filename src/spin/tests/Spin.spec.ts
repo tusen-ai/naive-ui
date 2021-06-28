5c7ab2dc6a9cfc9ca92aed986c288fa4ccb4a7a1
wanli.song@tusimple.ai <wanli.song@tusimple.ai>
2021-06-28 15:04:09 +0800

    test(spin): add icon slot test

--- src/spin/tests/Spin.spec.ts
+++ src/spin/tests/Spin.spec.ts
@@ -1,8 +1,22 @@
+import { h } from '@vue/runtime-core'
 import { mount } from '@vue/test-utils'
 import { NSpin } from '../index'
+import { Reload } from '@vicons/ionicons5'
+import { NIcon } from '../../icon'
 
 describe('n-spin', () => {
   it('should work with import on demand', () => {
     mount(NSpin)
+  })
+  it('should work with icon slot', () => {
+    const wrapper = mount(NSpin, {
+      slots: {
+        icon: () => h(NIcon, null, {
+          default: () => h(Reload)
+        })
+      }
+    })
+
+    expect(wrapper.find('.n-icon').exists()).toBe(true)
   })
 })
