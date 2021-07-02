274bcd5b7007012f2e759944035638225f2d4aa8
hhf <seafronthu@163.com>
2021-07-02 22:46:21 +0800

    修改为LoadingBarApi

--- src/loading-bar/index.ts
+++ src/loading-bar/index.ts
@@ -2,6 +2,6 @@ export { default as NLoadingBarProvider } from './src/LoadingBarProvider'
 export type {
   LoadingBarProviderInst,
   LoadingBarProviderProps,
-  LoadingBarApiInjection
+  LoadingBarApiInjection as LoadingBarApi
 } from './src/LoadingBarProvider'
 export { useLoadingBar } from './src/use-loading-bar'
