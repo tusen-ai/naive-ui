77d625bc6204a41c83a06e249f12a9b0d27873d3
ErinLee <761732114@qq.com>
2021-07-02 17:38:50 +0800

    fix: cursor setting fixed

--- src/ellipsis/src/Ellipsis.tsx
+++ src/ellipsis/src/Ellipsis.tsx
@@ -45,7 +45,8 @@ export default defineComponent({
     const ellipsisStyleRef = computed(() => {
       const { lineClamp } = props
       const { value: expanded } = expandedRef
-      const cursor = props.expandTrigger === 'click' ? 'pointer' : ''
+      const cursor =
+        !props.expandTrigger || props.expandTrigger === 'click' ? '' : 'pointer'
       if (lineClamp !== undefined) {
         return {
           cursor,
