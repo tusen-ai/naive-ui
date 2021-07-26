bf6c69be4a63d17daf9eecca2904a157e48e951d
XieZongChen <46394163+amadeus711@users.noreply.github.com>
2021-07-26 09:34:32 -0500

    feat(statistic): add statistic test (#670)

--- src/statistic/tests/Statistic.spec.ts
+++ src/statistic/tests/Statistic.spec.ts
@@ -5,4 +5,46 @@ describe('n-statistic', () => {
   it('should work with import on demand', () => {
     mount(NStatistic)
   })
+
+  it('should work with `label` prop', async () => {
+    const wrapper = mount(NStatistic, { props: { label: 'test' } })
+
+    expect(wrapper.find('.n-statistic__label').exists()).toBe(true)
+    expect(wrapper.find('.n-statistic__label').text()).toBe('test')
+  })
+
+  it('should work with `value` prop', async () => {
+    const wrapper = mount(NStatistic, { props: { value: 'test' } })
+
+    expect(wrapper.find('.n-statistic-value__content').exists()).toBe(true)
+    expect(wrapper.find('.n-statistic-value__content').text()).toBe('test')
+  })
+
+  it('should work with `default` slot', async () => {
+    const wrapper = mount(NStatistic, { slots: { default: () => 'test' } })
+
+    expect(wrapper.find('.n-statistic-value__content').exists()).toBe(true)
+    expect(wrapper.find('.n-statistic-value__content').text()).toBe('test')
+  })
+
+  it('should work with `label` slot', async () => {
+    const wrapper = mount(NStatistic, { slots: { label: () => 'test' } })
+
+    expect(wrapper.find('.n-statistic__label').exists()).toBe(true)
+    expect(wrapper.find('.n-statistic__label').text()).toBe('test')
+  })
+
+  it('should work with `prefix` slot', async () => {
+    const wrapper = mount(NStatistic, { slots: { prefix: () => 'test' } })
+
+    expect(wrapper.find('.n-statistic-value__prefix').exists()).toBe(true)
+    expect(wrapper.find('.n-statistic-value__prefix').text()).toBe('test')
+  })
+
+  it('should work with `suffix` slot', async () => {
+    const wrapper = mount(NStatistic, { slots: { suffix: () => 'test' } })
+
+    expect(wrapper.find('.n-statistic-value__suffix').exists()).toBe(true)
+    expect(wrapper.find('.n-statistic-value__suffix').text()).toBe('test')
+  })
 })
