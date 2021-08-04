12946303e93b78c0523137c665851b781912ed96
XieZongChen <46394163+amadeus711@users.noreply.github.com>
2021-08-04 01:28:49 -0500

    test(NResult): add test (#773)

--- src/result/tests/Result.spec.ts
+++ src/result/tests/Result.spec.ts
@@ -5,4 +5,41 @@ describe('n-result', () => {
   it('should work with import on demand', () => {
     mount(NResult)
   })
+
+  it('should work with `description` prop', async () => {
+    const wrapper = mount(NResult, {
+      props: { description: 'test-description' }
+    })
+    expect(wrapper.find('.n-result-header__description').exists()).toBe(true)
+    expect(wrapper.find('.n-result-header__description').text()).toBe(
+      'test-description'
+    )
+  })
+
+  it('should work with `title` prop', async () => {
+    const wrapper = mount(NResult, {
+      props: { title: 'test-title' }
+    })
+    expect(wrapper.find('.n-result-header__title').exists()).toBe(true)
+    expect(wrapper.find('.n-result-header__title').text()).toBe('test-title')
+  })
+
+  it('should work with `size` prop', async () => {
+    ;(['small', 'medium', 'large', 'huge'] as const).forEach((item) => {
+      const wrapper = mount(NResult, {
+        props: { size: item }
+      })
+      expect(wrapper.find('.n-result').attributes('style')).toMatchSnapshot()
+    })
+  })
+
+  it('should work with slots', async () => {
+    const wrapper = mount(NResult, {
+      slots: { default: () => 'test-default', footer: () => 'test-footer' }
+    })
+    expect(wrapper.find('.n-result-content').exists()).toBe(true)
+    expect(wrapper.find('.n-result-content').text()).toBe('test-default')
+    expect(wrapper.find('.n-result-footer').exists()).toBe(true)
+    expect(wrapper.find('.n-result-footer').text()).toBe('test-footer')
+  })
 })
