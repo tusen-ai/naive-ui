2c257ea8c29a19ee2a23c86fde995cd101263359
xiezongchen <952937570@qq.com>
2021-08-12 07:18:15 +0800

    test(utils): add css test

--- src/_utils/tests/Utils.spec.ts
+++ src/_utils/tests/Utils.spec.ts
@@ -1,5 +1,5 @@
-// import { mount } from '@vue/test-utils'
 import { createHoverColor, createPressedColor } from '../color'
+import { formatLength } from '..'
 
 describe('color', () => {
   it('should work with createHoverColor', () => {
@@ -12,5 +12,20 @@ describe('color', () => {
     expect(createPressedColor('rgb(42, 148, 125)')).toBe(
       'rgba(37, 130, 110, 1)'
     )
+  })
+})
+
+describe('css', () => {
+  it('should work with formatLength', () => {
+    expect(formatLength(7)).toBe('7px')
+    expect(formatLength(2, { offset: 3 })).toBe('5px')
+    expect(formatLength(3, { offset: 4, c: 2 })).toBe('14px')
+    expect(formatLength('3')).toBe('3px')
+    expect(formatLength('3', { attachPx: false })).toBe('3')
+    expect(formatLength('2', { offset: 3 })).toBe('5px')
+    expect(formatLength('3', { offset: 4, c: 2 })).toBe('14px')
+    expect(formatLength('4px')).toBe('4px')
+    expect(formatLength('2px', { offset: 3 })).toBe('5px')
+    expect(formatLength('3px', { offset: 4, c: 2 })).toBe('14px')
   })
 })
