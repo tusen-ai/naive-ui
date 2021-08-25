411ab9316ad33461711a4cdbe34d6ce72bf82d46
马泽坤 <984757534@qq.com>
2021-08-25 18:59:30 +0800

    test(tabs): add some test case

--- src/tabs/tests/Tabs.spec.ts
+++ src/tabs/tests/Tabs.spec.ts
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils'
 import { h } from 'vue'
 import { NTabPane, NTabs } from '../index'
+import { AddIcon } from '../../_internal/icons'
 
 describe('n-tabs', () => {
   it('should work with import on demand', () => {
@@ -39,5 +40,39 @@ describe('n-tabs', () => {
           })
       }
     })
+  })
+
+  it('should show AddIcon with `addable` prop', () => {
+    const wrapper = mount(NTabs, {
+      props: {
+        type: 'card',
+        addable: true
+      }
+    })
+
+    expect(wrapper.findComponent(AddIcon).exists()).toBe(true)
+  })
+
+  it('should work with `justify-content` prop', async () => {
+    const wrapper = mount(NTabs, {
+      props: {
+        defaultValue: 'a'
+      }
+    })
+
+    await wrapper.setProps({ justifyContent: 'space-between' })
+    expect(
+      wrapper.find('.n-tabs-wrapper').attributes('style')
+    ).toMatchSnapshot()
+
+    await wrapper.setProps({ justifyContent: 'space-around' })
+    expect(
+      wrapper.find('.n-tabs-wrapper').attributes('style')
+    ).toMatchSnapshot()
+
+    await wrapper.setProps({ justifyContent: 'space-evenly' })
+    expect(
+      wrapper.find('.n-tabs-wrapper').attributes('style')
+    ).toMatchSnapshot()
   })
 })
