9e46d5187961f16f1dc22c6a0c226be47c699571
XieZongChen <46394163+amadeus711@users.noreply.github.com>
2021-09-02 18:35:07 -0500

    test(cascader): add test (#1061)

--- src/cascader/tests/Cascader.spec.ts
+++ src/cascader/tests/Cascader.spec.ts
@@ -5,4 +5,34 @@ describe('n-cascader', () => {
   it('should work with import on demand', () => {
     mount(NCascader)
   })
+
+  it('should work with `disabled` prop', async () => {
+    const wrapper = mount(NCascader)
+    expect(wrapper.find('.n-base-selection').classes()).not.toContain(
+      'n-base-selection--disabled'
+    )
+
+    await wrapper.setProps({ disabled: true })
+    expect(wrapper.find('.n-base-selection').classes()).toContain(
+      'n-base-selection--disabled'
+    )
+  })
+
+  it('should work with `size` prop', async () => {
+    ;(['small', 'medium', 'large'] as const).forEach((i) => {
+      const wrapper = mount(NCascader, { props: { size: i } })
+      expect(
+        wrapper.find('.n-base-selection').attributes('style')
+      ).toMatchSnapshot()
+    })
+  })
+
+  it('should work with `placeholder` prop', async () => {
+    const wrapper = mount(NCascader, {
+      props: { placeholder: 'test-placeholder' }
+    })
+    expect(wrapper.find('.n-base-selection-placeholder').text()).toBe(
+      'test-placeholder'
+    )
+  })
 })
