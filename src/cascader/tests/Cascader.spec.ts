911d9ac52207534950c41a14d213b7ce70a4f6d1
xiezongchen <952937570@qq.com>
2021-09-06 10:17:11 +0800

    test(cascader): update test

--- src/cascader/tests/Cascader.spec.ts
+++ src/cascader/tests/Cascader.spec.ts
@@ -97,4 +97,51 @@ describe('n-cascader', () => {
     expect(wrapper.find('.n-base-selection-tags').exists()).toBe(true)
     expect(wrapper.find('.n-base-selection-label').exists()).not.toBe(true)
   })
+
+  it('should work with `label-field` `value-field` `children-field` props', async () => {
+    const wrapper = mount(NCascader, {
+      props: {
+        options: [
+          {
+            whateverLabel: 'Rubber Soul',
+            whateverValue: 'Rubber Soul',
+            whateverChildren: [
+              {
+                whateverLabel:
+                  "Everybody's Got Something to Hide Except Me and My Monkey",
+                whateverValue:
+                  "Everybody's Got Something to Hide Except Me and My Monkey"
+              }
+            ]
+          }
+        ] as any,
+        'label-field': 'whateverLabel',
+        'value-field': 'whateverValue',
+        'children-field': 'whateverChildren',
+        'default-value':
+          "Everybody's Got Something to Hide Except Me and My Monkey"
+      }
+    })
+    expect(wrapper.find('.n-base-selection-label').text()).toBe(
+      "Rubber Soul / Everybody's Got Something to Hide Except Me and My Monkey"
+    )
+  })
+
+  // it('should work with `leaf-only` prop', async () => {
+  //   const wrapper = mount(NCascader, {
+  //     attachTo: document.body,
+  //     props: { options: getOptions(), virtualScroll: false }
+  //   })
+  //   await wrapper.setProps({ show: true })
+  //   await nextTick()
+  //   console.log(
+  //     document.querySelector('.n-cascader-submenu-wrapper')?.innerHTML
+  //   )
+
+  //   expect(document.querySelector('.n-checkbox')).not.toEqual(null)
+
+  //   await wrapper.setProps({ leafOnly: true })
+
+  //   expect(document.querySelector('.n-checkbox')).toEqual(null)
+  // })
 })
