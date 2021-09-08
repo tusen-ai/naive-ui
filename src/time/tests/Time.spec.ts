ee5103c14e30f6732aafd1e8a77da116447e2d7a
songjian <1778651752@qq.com>
2021-09-08 15:47:27 +0800

    test(time): Update test

--- src/time/tests/Time.spec.ts
+++ src/time/tests/Time.spec.ts
@@ -8,46 +8,32 @@ describe('n-time', () => {
     mount(NTime)
   })
 
-  it('unix is true', () => {
+  it('should work with `unix` prop', async () => {
     const wrapper = mount(NTime, { props: { time: 0, unix: true } })
+    expect(wrapper.find('time').text()).toContain('1970-01-01 12:00:00')
+    await wrapper.setProps({ time: date })
     expect(wrapper.find('time').text()).toContain('1970-01-01 08:00:00')
   })
 
-  it('unix is false', () => {
-    const wrapper = mount(NTime, { props: { time: date } })
-    expect(wrapper.find('time').text()).toContain('1970-01-01 08:00:00')
-  })
-
-  it('format is string', () => {
+  it('should work with `format` prop', () => {
     const wrapper = mount(NTime, {
       props: { time: date, format: 'yyyy/MM/dd hh:mm:ss' }
     })
     expect(wrapper.find('time').text()).toContain('1970/01/01 08:00:00')
   })
 
-  it('type value is relative and unix is true', () => {
-    const wrapper = mount(NTime, {
-      props: { time: 0, type: 'relative', unix: true }
-    })
+  it('should work with `type` prop', async () => {
+    const wrapper = mount(NTime, { props: { time: date, type: 'date' } })
+    expect(wrapper.find('time').text()).toContain('1970-01-01')
+    await wrapper.setProps({ time: date, type: 'datetime' })
+    expect(wrapper.find('time').text()).toContain('1970-01-01 08:00:00')
+    await wrapper.setProps({ time: date, type: 'relative' })
+    expect(wrapper.find('time').text()).toContain('over 51 years ago')
+    await wrapper.setProps({ time: 0, type: 'relative', unix: true })
     expect(wrapper.find('time').text()).toContain('about 51687 years ago')
   })
 
-  it('type value is relative and unix is false', () => {
-    const wrapper = mount(NTime, { props: { time: date, type: 'relative' } })
-    expect(wrapper.find('time').text()).toContain('over 51 years ago')
-  })
-
-  it('type value is date', () => {
-    const wrapper = mount(NTime, { props: { time: date, type: 'date' } })
-    expect(wrapper.find('time').text()).toContain('1970-01-01')
-  })
-
-  it('type value is datetime', () => {
-    const wrapper = mount(NTime, { props: { time: date, type: 'datetime' } })
-    expect(wrapper.find('time').text()).toContain('1970-01-01 08:00:00')
-  })
-
-  it('text is true', () => {
+  it('should work with `text` prop', () => {
     const wrapper = mount(NTime, { props: { time: date, text: true } })
     expect(wrapper.text()).toContain('1970-01-01 08:00:00')
   })
