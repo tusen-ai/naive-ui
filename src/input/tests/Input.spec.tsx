18a0e3bb2a447d26cd9d2034bccad48f94e03a66
xiezongchen <952937570@qq.com>
2021-09-14 16:44:00 +0800

    feat(input): add test

--- src/input/tests/Input.spec.tsx
+++ src/input/tests/Input.spec.tsx
@@ -5,6 +5,7 @@ describe('n-input', () => {
   it('should work with import on demand', () => {
     mount(NInput)
   })
+
   it('should call input callbacks', async () => {
     const onUpdateValue = jest.fn()
     const wrapper = mount(NInput, {
@@ -16,6 +17,7 @@ describe('n-input', () => {
     await wrapper.find('input').trigger('input')
     expect(onUpdateValue).toHaveBeenCalledWith('cool')
   })
+
   it('`loading` prop', async () => {
     const wrapper = mount(NInput)
     expect(wrapper.find('.n-input__suffix').exists()).toBe(false)
@@ -27,6 +29,67 @@ describe('n-input', () => {
     expect(wrapper.find('.n-base-loading__icon').exists()).toBe(true)
     wrapper.unmount()
   })
+
+  it('should work with `clearable` prop', async () => {
+    const wrapper = mount(NInput)
+    expect(wrapper.find('.n-base-clear').exists()).not.toBe(true)
+    await wrapper.setProps({
+      clearable: true
+    })
+    expect(wrapper.find('.n-base-clear').exists()).toBe(true)
+    wrapper.unmount()
+  })
+
+  it('should work with `disabled` prop', async () => {
+    const wrapper = mount(NInput)
+    expect(wrapper.find('.n-input').classes()).not.toContain(
+      'n-input--disabled'
+    )
+    await wrapper.setProps({
+      disabled: true
+    })
+    expect(wrapper.find('.n-input').classes()).toContain('n-input--disabled')
+    wrapper.unmount()
+  })
+
+  it('should work with `placeholder` prop', async () => {
+    const wrapper = mount(NInput)
+    expect(wrapper.find('input').attributes('placeholder')).toBe('Please Input')
+    await wrapper.setProps({
+      placeholder: 'test-placeholder'
+    })
+    expect(wrapper.find('input').attributes('placeholder')).toBe(
+      'test-placeholder'
+    )
+    wrapper.unmount()
+  })
+
+  it('should work with `readonly` prop', async () => {
+    const wrapper = mount(NInput)
+    expect(wrapper.find('input').attributes('readonly')).not.toBe('')
+
+    await wrapper.setProps({ readonly: true })
+    expect(wrapper.find('input').attributes('readonly')).toBe('')
+    wrapper.unmount()
+  })
+
+  it('should work with `round` prop', async () => {
+    const wrapper = mount(NInput)
+    expect(wrapper.find('.n-input').classes()).not.toContain('n-input--round')
+
+    await wrapper.setProps({ round: true })
+    expect(wrapper.find('.n-input').classes()).toContain('n-input--round')
+    wrapper.unmount()
+  })
+
+  it('should work with `size` prop', async () => {
+    ;(['small', 'medium', 'large'] as const).forEach((size) => {
+      const wrapper = mount(NInput, { props: { size: size } })
+      expect(wrapper.find('.n-input').attributes('style')).toMatchSnapshot()
+      wrapper.unmount()
+    })
+  })
+
   it('should work with `type` prop', async () => {
     const wrapper = mount(NInput)
     await wrapper.setProps({ type: 'text' })
