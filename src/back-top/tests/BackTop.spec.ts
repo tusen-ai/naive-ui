bd938ce416a4b78816bf5e9ed3c2314ef7c278d7
songjian <1778651752@qq.com>
2021-09-16 15:10:58 +0800

    test(backtop): Add backtop component test

--- src/back-top/tests/BackTop.spec.ts
+++ src/back-top/tests/BackTop.spec.ts
@@ -1,8 +1,24 @@
+import { h } from 'vue'
 import { mount } from '@vue/test-utils'
 import { NBackTop } from '../index'
 
 describe('n-back-top', () => {
   it('should work with import on demand', () => {
     mount(NBackTop)
+  })
+
+  it('should work with `show` prop', async () => {
+    const wrapper = mount(NBackTop, {
+      props: {
+        show: true
+      },
+      slots: {
+        default: () => {
+          return h('div', {}, '返回顶部')
+        }
+      }
+    })
+
+    expect(wrapper.html()).toContain('teleport start')
   })
 })
