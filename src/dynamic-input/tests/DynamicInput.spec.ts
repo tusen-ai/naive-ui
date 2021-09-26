e936359b4eecd24719d0255edab3d85725178409
songjian <1778651752@qq.com>
2021-09-26 17:50:54 +0800

    test(dynamic-input): Add dynamic-input component test

--- src/dynamic-input/tests/DynamicInput.spec.ts
+++ src/dynamic-input/tests/DynamicInput.spec.ts
@@ -5,4 +5,14 @@ describe('n-dynamic-input', () => {
   it('should work with import on demand', () => {
     mount(NDynamicInput)
   })
+
+  it('should work with `value`', () => {
+    const wrapper = mount(NDynamicInput, {
+      props: {
+        value: ['aaa']
+      }
+    })
+
+    expect(wrapper.html()).toContain('data-key="0"')
+  })
 })
