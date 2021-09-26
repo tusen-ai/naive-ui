f4be21244efb36b69a4e96f122262ce649d8530b
songjianet <1778651752@qq.com>
2021-09-26 22:36:34 +0800

    test(dynamic-input): Add  test (#1242)
    * test(dynamic-input): Add dynamic-input component test
    
    * docs(dynamic-input): Update dynamic-input component test

--- src/dynamic-input/tests/DynamicInput.spec.ts
+++ src/dynamic-input/tests/DynamicInput.spec.ts
@@ -5,4 +5,16 @@ describe('n-dynamic-input', () => {
   it('should work with import on demand', () => {
     mount(NDynamicInput)
   })
+
+  it('should work with `value`', async () => {
+    const wrapper = mount(NDynamicInput, {
+      props: {
+        value: ['aaa']
+      }
+    })
+
+    const inputEl = await wrapper.find('input')
+    expect(inputEl.element.value).toEqual('aaa')
+    expect(wrapper.html()).toContain('data-key="0"')
+  })
 })
