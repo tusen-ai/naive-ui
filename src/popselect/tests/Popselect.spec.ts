00c0e25f69bb9d728508a14074137ddddcaece45
songjianet <1778651752@qq.com>
2021-09-29 13:54:49 +0800

    test(popselect): Add popselect component test (#1260)

--- src/popselect/tests/Popselect.spec.ts
+++ src/popselect/tests/Popselect.spec.ts
@@ -1,3 +1,4 @@
+import { h } from 'vue'
 import { mount } from '@vue/test-utils'
 import { NPopselect } from '../index'
 
@@ -8,5 +9,30 @@ describe('n-popselect', () => {
         default: () => 'star kirby'
       }
     })
+  })
+
+  it('should be active after clicked', async () => {
+    const wrapper = mount(NPopselect, {
+      attachTo: document.body,
+      props: {
+        trigger: 'click',
+        value: '',
+        options: [
+          { label: 'Drive My Car', value: 'Drive My Car' },
+          { label: 'Norwegian Wood', value: 'Norwegian Wood' }
+        ]
+      },
+      slots: {
+        default: () => h('button', { class: 'test-btn' }, '点击打开')
+      }
+    })
+
+    await wrapper.find('.test-btn').trigger('click')
+    expect(document.querySelector('.n-base-select-menu')).not.toEqual(null)
+
+    await wrapper.find('.test-btn').trigger('click')
+    expect(document.querySelector('.n-base-select-menu')).toEqual(null)
+
+    wrapper.unmount()
   })
 })
