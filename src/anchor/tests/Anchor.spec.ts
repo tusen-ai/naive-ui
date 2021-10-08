a9c1b9db2596807375df6dbe383fa6abcf9d41ef
songjian <1778651752@qq.com>
2021-10-08 10:57:31 +0800

    test(cascader): Update cascader component test

--- src/anchor/tests/Anchor.spec.ts
+++ src/anchor/tests/Anchor.spec.ts
@@ -31,6 +31,20 @@ describe('n-anchor', () => {
     expect(wrapper.find('.n-affix').exists()).toBe(true)
   })
 
+  it('should work with `type` prop', async () => {
+    const wrapper = mount(NAnchor, {
+      props: {
+        type: 'rail'
+      }
+    })
+
+    expect(wrapper.find('.n-anchor--show-rail').exists()).toBe(true)
+
+    await wrapper.setProps({ type: 'block' })
+
+    expect(wrapper.find('.n-anchor--block').exists()).toBe(true)
+  })
+
   it('should work with `title` and `href` prop', async () => {
     const wrapper = mount(NAnchor, {
       props: {
