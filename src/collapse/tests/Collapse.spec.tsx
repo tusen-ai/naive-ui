e2c59143f3a9146b35fc07d63d93a33ad0c1f963
songjian <1778651752@qq.com>
2021-10-14 15:58:13 +0800

    test(collapse): Update collapse component test

--- src/collapse/tests/Collapse.spec.tsx
+++ src/collapse/tests/Collapse.spec.tsx
@@ -82,28 +82,37 @@ describe('n-collapse', () => {
   })
 
   it('should work with nested structure', async () => {
-    mount(NCollapse, {
+    const wrapper = mount(NCollapse, {
       slots: {
         default: () =>
           h(
             NCollapseItem,
-            { name: '1', title: 'test1' },
+            { name: '1' },
             {
               default: () =>
                 h(NCollapse, null, {
-                  default: () => h(NCollapseItem, { name: '2', title: 'test2' })
+                  default: () => h(NCollapseItem, { name: '2' })
                 })
             }
           )
       }
     })
 
-    // todo: test display-directive
-    // I wanted to test this function, but I was bothered by the <transition-stub>
+    await wrapper.find('.n-collapse-item__header-main').trigger('click')
+    expect(wrapper.find('.n-collapse-item__header').classes()).toContain(
+      'n-collapse-item__header--active'
+    )
+    await wrapper
+      .find('transition-stub')
+      .find('.n-collapse-item__header-main')
+      .trigger('click')
+    expect(
+      wrapper.find('transition-stub').find('.n-collapse-item__header').classes()
+    ).toContain('n-collapse-item__header--active')
   })
 
   it('should work with `display-directive` prop', async () => {
-    mount(NCollapse, {
+    const wrapper = mount(NCollapse, {
       props: {
         displayDirective: 'show'
       },
@@ -111,13 +120,33 @@ describe('n-collapse', () => {
         default: () =>
           h(
             NCollapseItem,
-            { name: '1', title: 'test' },
+            { name: '1' },
             { default: () => h('div', null, { default: () => 'test' }) }
           )
       }
-      // todo: test display-directive
-      // I wanted to test this function, but I was bothered by the <transition-stub>
     })
+
+    await wrapper.find('.n-collapse-item__header-main').trigger('click')
+    await wrapper.find('.n-collapse-item__header-main').trigger('click')
+    expect(
+      wrapper
+        .find('transition-stub')
+        .find('.n-collapse-item__content-wrapper')
+        .attributes('style')
+    ).toBe('display: none;')
+
+    await wrapper.setProps({
+      displayDirective: 'if'
+    })
+
+    await wrapper.find('.n-collapse-item__header-main').trigger('click')
+    await wrapper.find('.n-collapse-item__header-main').trigger('click')
+    expect(
+      wrapper
+        .find('transition-stub')
+        .find('.n-collapse-item__content-wrapper')
+        .exists()
+    ).toBe(false)
   })
 
   it('should work with `on-item-header-click` prop', async () => {
@@ -192,5 +221,16 @@ describe('n-collapse', () => {
     })
     expect(wrapper.find('.ci1').isVisible()).toEqual(true)
     expect(wrapper.find('.ci2').exists()).toEqual(false)
+  })
+
+  it('should work with collapseItem component `title` prop', async () => {
+    const wrapper = mount(NCollapse, {
+      slots: {
+        default: () => <NCollapseItem title="test"></NCollapseItem>
+      }
+    })
+
+    await wrapper.find('.n-collapse-item__header-main').trigger('click')
+    expect(wrapper.find('.n-collapse-item__header-main').text()).toBe('test')
   })
 })
