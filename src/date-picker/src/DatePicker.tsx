140623cdc88efe1ee858de7b0e9380fe5805547c
07akioni <07akioni2@gmail.com>
2021-10-16 02:08:31 +0800

    Update src/date-picker/src/DatePicker.tsx
    Co-authored-by: Mr.Bai <935196116@qq.com>

--- src/date-picker/src/DatePicker.tsx
+++ src/date-picker/src/DatePicker.tsx
@@ -94,7 +94,7 @@ const datePickerProps = {
   size: String as PropType<'small' | 'medium' | 'large'>,
   type: {
     type: String as PropType<
-    'date' | 'datetime' | 'daterange' | 'datetimerange' | 'month' | 'year'
+   keyof typeof DATE_FORMAT
     >,
     default: 'date'
   },
