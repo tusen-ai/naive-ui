49c0a243798b3d9d6d120ddeb2cb55eef81e8cea
zoomdong <1344492820@qq.com>
2021-10-23 00:22:10 +0800

    workflow: move to pnpm

--- jest.config.js
+++ jest.config.js
@@ -153,11 +153,12 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.(j|t)sx?$': 'babel-jest'
+    '^.+\\.(jsx|js)?$': 'babel-jest',
+    '^.+\\.(tsx|ts)?$': 'ts-jest'
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/.pnpm/(?!lodash-es)'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
