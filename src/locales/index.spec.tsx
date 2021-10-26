0e8486dadfd4131ac50ecef45dfa03d5c8e0e7fa
Christopher Reeve <45350572+zynth17@users.noreply.github.com>
2021-10-27 00:19:08 +0700

    add idonesian locale (#1438)

--- src/locales/index.spec.tsx
+++ src/locales/index.spec.tsx
@@ -5,12 +5,14 @@ import {
   zhCN,
   enUS,
   ukUA,
-  jaJP,
+	jaJP,
+  idID,
   dateEnUS,
   dateZhCN,
   dateRuRU,
   dateUkUA,
-  dateJaJP,
+	dateJaJP,
+  dateIdID,
   NConfigProvider,
   NDateLocale,
   NLocale,
@@ -69,6 +71,14 @@ describe('locale', () => {
         props: {
           dateLocale: dateJaJP,
           locale: jaJP
+        }
+      }).html()
+    ).toMatchSnapshot()
+    expect(
+      mount(Wrapper, {
+        props: {
+          dateLocale: dateIdID,
+          locale: idID
         }
       }).html()
     ).toMatchSnapshot()
