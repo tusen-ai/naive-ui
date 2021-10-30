f1fd51e0ed2bab0836ce6ffbd4cedc4fa8630863
XieZongChen <46394163+amadeus711@users.noreply.github.com>
2021-10-30 01:12:38 -0500

    test(input): update test (#1483)

--- src/input/tests/Input.spec.tsx
+++ src/input/tests/Input.spec.tsx
@@ -83,6 +83,15 @@ describe('n-input', () => {
     wrapper.unmount()
   })
 
+  it('should work with `rows` prop', async () => {
+    const wrapper = mount(NInput, { props: { type: 'textarea' } })
+    expect(wrapper.find('textarea').attributes('rows')).toBe('3')
+
+    await wrapper.setProps({ type: 'textarea', rows: 5 })
+    expect(wrapper.find('textarea').attributes('rows')).toBe('5')
+    wrapper.unmount()
+  })
+
   it('should work with `size` prop', async () => {
     ;(['small', 'medium', 'large'] as const).forEach((size) => {
       const wrapper = mount(NInput, { props: { size: size } })
