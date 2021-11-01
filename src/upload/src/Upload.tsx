00acc79d9d72098da418674b94b2103c57187102
Kigoal <i@kigoal.cc>
2021-11-01 13:38:42 +0800

    feat(upload): add createThumbnailUrl parameter fileInfo

--- src/upload/src/Upload.tsx
+++ src/upload/src/Upload.tsx
@@ -447,7 +447,7 @@ export default defineComponent({
       const { createThumbnailUrl } = props
 
       return createThumbnailUrl
-        ? await createThumbnailUrl(file.file as File)
+        ? await createThumbnailUrl(file.file as File, file)
         : await createImageDataUrl(file.file as File)
     }
     const cssVarsRef = computed(() => {
