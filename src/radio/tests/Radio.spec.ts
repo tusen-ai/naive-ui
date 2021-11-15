43f6dd1cb2692f1b6fc3c93bbf038e3a21ec7072
XieZongChen <952937570@qq.com>
2021-11-15 13:59:32 +0800

    test(radio): update test

--- src/radio/tests/Radio.spec.ts
+++ src/radio/tests/Radio.spec.ts
@@ -44,14 +44,10 @@ describe('n-radio', () => {
   })
 
   it('should work with `size` prop', async () => {
-    const wrapper = mount(NRadio, { props: { size: 'small' } })
-    expect(wrapper.find('.n-radio').attributes('style')).toMatchSnapshot()
-
-    await wrapper.setProps({ size: 'medium' })
-    expect(wrapper.find('.n-radio').attributes('style')).toMatchSnapshot()
-
-    await wrapper.setProps({ size: 'large' })
-    expect(wrapper.find('.n-radio').attributes('style')).toMatchSnapshot()
+    ;(['small', 'medium', 'large'] as const).forEach((size) => {
+      const wrapper = mount(NRadio, { props: { size: size } })
+      expect(wrapper.find('.n-radio').attributes('style')).toMatchSnapshot()
+    })
   })
 })
 
