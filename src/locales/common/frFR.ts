858f94f1545bd52845d12a975fb88adebcd74ae2
07akioni <07akioni2@gmail.com>
2021-12-07 01:19:45 +0800

    fix: fr locale

--- src/locales/common/frFR.ts
+++ src/locales/common/frFR.ts
@@ -25,6 +25,8 @@ const frFR: NLocale = {
     yearFormat: 'yyyy',
     monthFormat: 'MMM',
     dayFormat: 'eeeeee',
+    dateFormat: 'dd-MM-yyyy',
+    dateTimeFormat: 'dd-MM-yyyy HH:mm:ss',
     clear: 'Effacer',
     now: 'Maintenant',
     confirm: 'Confirmer',
