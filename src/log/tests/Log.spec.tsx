d129a2fd4cdd0a6703142bdc9ab5bbfd7b03f18b
chenyinan <cyntimetravel@gmail.com>
2021-12-18 13:07:44 +0800

    feat(log): undo jest change

--- src/log/tests/Log.spec.tsx
+++ src/log/tests/Log.spec.tsx
@@ -20,7 +20,7 @@ describe('n-log', () => {
     const wrapper = mount(NLog, { props: { fontSize } })
 
     expect(wrapper.find('.n-code').attributes('style')).toContain(
-      `--n-font-size: ${fontSize}px`
+      `--font-size: ${fontSize}px`
     )
   })
 
