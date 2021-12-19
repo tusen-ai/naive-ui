c99d69b4b7907a7b1907e432faff10175317757a
caoyugang <1442770751@qq.com>
2021-12-19 20:46:42 +0800

    fix: update fn name

--- scripts/utils/loader.js
+++ scripts/utils/loader.js
@@ -42,40 +42,49 @@ function getPartsOfMdDemo (tokens) {
 }
 
 async function loadFile (filepath) {
-  if (fs.existsSync(filepath)) { return await fs.readFile(filepath, 'utf-8') }
+  if (fs.existsSync(filepath)) {
+    return await fs.readFile(filepath, 'utf-8')
+  }
   return undefined
 }
 
-async function loadAllFile (filePathArr) {
+async function loadAllMdFile (filePathArr) {
   const filesArr = []
   for (let i = 0; i < filePathArr.length; i++) {
     const filePath = filePathArr[i]
     if (fs.existsSync(filePath)) {
       const files = await fs.readdir(filePath)
-      const filesObjArr = files.filter(file => fileRegex.test(file)).map(file => {
-        const index = file.lastIndexOf('.')
-        return {
-          path: `${filePath}/${file}`,
-          dir: filePath,
-          file: file,
-          name: file.slice(0, index)
-        }
-      })
+      const filesObjArr = files
+        .filter((file) => fileRegex.test(file))
+        .map((file) => {
+          const index = file.lastIndexOf('.')
+          return {
+            path: `${filePath}/${file}`,
+            dir: filePath,
+            file: file,
+            name: file.slice(0, index)
+          }
+        })
       filesArr.push(...filesObjArr)
     }
   }
-  handleFile(filesArr)
+  return filesArr
 }
 
 async function updateIndexEntryDemo (file) {
-  let indexFileContent = await loadFile(path.resolve(file.dir, './index.demo-entry.md'))
+  let indexFileContent = await loadFile(
+    path.resolve(file.dir, './index.demo-entry.md')
+  )
   const index = file.name.indexOf('.')
   const name = file.name.slice(0, index)
   indexFileContent = indexFileContent.replace(name, `${name}.vue`)
-  await fs.writeFileSync(path.resolve(file.dir, './index.demo-entry.md'), indexFileContent)
+  await fs.writeFileSync(
+    path.resolve(file.dir, './index.demo-entry.md'),
+    indexFileContent
+  )
 }
 
-async function handleFile (files) {
+async function transformMdToVueAndUpdateEntryFile (files) {
   for (const file of files) {
     const fileString = await loadFile(file.path)
     const tokens = marked.lexer(fileString)
@@ -104,8 +113,11 @@ ${parts.style}
     }
     await fs.remove(file.path)
     await fs.ensureDir(file.dir)
-    await fs.writeFileSync(path.resolve(file.dir, `./${file.name}.vue`), vueDemo)
-    // 应该可以统一一起修改
+    await fs.writeFileSync(
+      path.resolve(file.dir, `./${file.name}.vue`),
+      vueDemo
+    )
+    // should be able to be modified together
     await updateIndexEntryDemo(file)
   }
 }
@@ -113,8 +125,13 @@ ${parts.style}
 const COMPONENT_ROOT = path.resolve(__dirname, '../../src')
 
 async function convertFilesByComponentName (componentName) {
-  const folders = ['zhCN', 'enUS'].map(item => path.resolve(COMPONENT_ROOT, `${componentName}/demos/${item}`))
-  if (folders.length) { return await loadAllFile(folders) }
+  const folders = ['zhCN', 'enUS'].map((item) =>
+    path.resolve(COMPONENT_ROOT, `${componentName}/demos/${item}`)
+  )
+  if (folders.length) {
+    const files = await loadAllMdFile(folders)
+    transformMdToVueAndUpdateEntryFile(files)
+  }
 }
 
 module.exports = {
