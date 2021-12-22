993da4a0162ee04e75614023de23612e288443af
XieZongChen <952937570@qq.com>
2021-12-22 14:37:49 +0800

    feat(mention): mention uses n as CSS vars prefix

--- src/mention/src/styles/index.cssr.ts
+++ src/mention/src/styles/index.cssr.ts
@@ -1,11 +1,11 @@
 import fadeInScaleUp from '../../../_styles/transitions/fade-in-scale-up.cssr'
 import { c, cB } from '../../../_utils/cssr'
 
-// --menu-box-shadow
+// --n-menu-box-shadow
 export default c([
   cB('mention', 'width: 100%; z-index: auto; position: relative;'),
   cB('mention-menu', `
-    box-shadow: var(--menu-box-shadow);
+    box-shadow: var(--n-menu-box-shadow);
   `, [
     fadeInScaleUp()
   ])
