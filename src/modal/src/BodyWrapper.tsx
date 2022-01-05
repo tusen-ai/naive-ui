cb0692357a1a631127a581784d4830b8a0ab0df8
nooooooom <nooooooom@163.com>
2022-01-05 13:34:26 +0800

    chore: ignore ts error

--- src/modal/src/BodyWrapper.tsx
+++ src/modal/src/BodyWrapper.tsx
@@ -237,7 +237,7 @@ export default defineComponent({
                                     themeOverrides={
                                       this.mergedTheme.peerOverrides.Dialog
                                     }
-                                    {...keep(this.$props, dialogPropKeys)}
+                                    {...keep(this.$props, dialogPropKeys) as any}
                                     aria-modal="true"
                                   >
                                     {$slots}
@@ -254,7 +254,7 @@ export default defineComponent({
                                     themeOverrides={
                                       this.mergedTheme.peerOverrides.Card
                                     }
-                                    {...keep(this.$props, cardBasePropKeys)}
+                                    {...keep(this.$props, cardBasePropKeys) as any}
                                     aria-modal="true"
                                     role="dialog"
                                   >
