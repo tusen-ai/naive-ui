47358e8fa4a65b3eb4b91f8fa21ca03ff3f3d565
Julian Hundeloh <julian.hundeloh+github@gmail.com>
2022-01-11 16:05:20 +0000

    style: fix lint

--- src/color-picker/src/interface.ts
+++ src/color-picker/src/interface.ts
@@ -4,5 +4,5 @@ export type OnUpdateValue = (value: string & null) => void
 export type OnUpdateValueImpl = (value: string | null) => void
 
 export type RenderLabel = (
-    value: string | null
-  ) => VNodeChild
+  value: string | null
+) => VNodeChild
