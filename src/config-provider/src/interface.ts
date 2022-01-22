a5a88ea7f30d3e93ca3f8242cc89d472b2829268
Julian Hundeloh <jaulz@users.noreply.github.com>
2022-01-22 14:26:53 +0100

    feat: export ThemeCommonVarsExtension and make type augmentable (#2191)
    * feat: export ThemeCommonVars and make type augmentable
    
    * style: fix lint
    
    * fix: extend return type of useThemeVars

--- src/config-provider/src/interface.ts
+++ src/config-provider/src/interface.ts
@@ -2,12 +2,17 @@ import type { ThemeCommonVars } from '../../_styles/common'
 import type { ExtractThemeOverrides } from '../../_mixins/use-theme'
 import type { GlobalThemeWithoutCommon } from './internal-interface'
 
+export { ThemeCommonVars }
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ThemeCommonVarsExtension {}
+
 export interface GlobalTheme extends GlobalThemeWithoutCommon {
   common?: ThemeCommonVars
 }
 
 export type GlobalThemeOverrides = {
-  common?: Partial<ThemeCommonVars>
+  common?: Partial<ThemeCommonVars & ThemeCommonVarsExtension>
 } & {
   [key in keyof GlobalThemeWithoutCommon]?: ExtractThemeOverrides<
   GlobalThemeWithoutCommon[key]
