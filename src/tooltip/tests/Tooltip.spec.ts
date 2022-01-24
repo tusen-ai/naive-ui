04ba253c2df66bfd0f4cf713a3b273c63f840955
XieZongChen <46394163+amadeus711@users.noreply.github.com>
2022-01-24 16:42:28 +0800

    test(tooltip): update test (#2255)

--- src/tooltip/tests/Tooltip.spec.ts
+++ src/tooltip/tests/Tooltip.spec.ts
@@ -9,4 +9,19 @@ describe('n-tooltip', () => {
       }
     })
   })
+
+  it('should work with `show` props', async () => {
+    const wrapper = mount(NTooltip, {
+      slots: {
+        default: () => 'test-default',
+        trigger: () => 'test-trigger'
+      },
+      attachTo: document.body
+    })
+    expect(document.querySelector('.n-tooltip')).toEqual(null)
+
+    await wrapper.setProps({ show: true })
+    expect(document.querySelector('.n-tooltip')).not.toEqual(null)
+    wrapper.unmount()
+  })
 })
