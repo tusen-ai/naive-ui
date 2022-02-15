f3215061908f72a55f34858705d5caab9e71e5bb
nooooooom <nooooooom@163.com>
2022-02-15 23:40:21 +0800

    docs(Carouse): md to vue

--- src/carousel/demos/enUS/hover.demo.vue
+++ src/carousel/demos/enUS/hover.demo.vue
@@ -1,46 +1,34 @@
-# 居中
+<markdown>
+# Hover
 
-设定 `centered-slides` 来将所有的 `slide` 居中显示，这仅在 `effect` 为 `slide` 以及 `card` 的时候有用。
+Set the `trigger` to `hover` to trigger the switch when the mouse passes over the dots.
+</markdown>
 
-```html
-<n-carousel
-  :space-between="30"
-  :loop="false"
-  slides-per-view="auto"
-  centered-slides
-  draggable
->
-  <n-carousel-item style="width: 30%">
+<template>
+  <n-carousel trigger="hover">
     <img
       class="carousel-img"
       src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg"
-    />
-  </n-carousel-item>
-  <n-carousel-item style="width: 20%">
+    >
     <img
       class="carousel-img"
       src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg"
-    />
-  </n-carousel-item>
-  <n-carousel-item style="width: 30%">
+    >
     <img
       class="carousel-img"
       src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg"
-    />
-  </n-carousel-item>
-  <n-carousel-item style="width: 40%">
+    >
     <img
       class="carousel-img"
       src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg"
-    />
-  </n-carousel-item>
-</n-carousel>
-```
+    >
+  </n-carousel>
+</template>
 
-```css
+<style>
 .carousel-img {
   width: 100%;
   height: 240px;
   object-fit: cover;
 }
-```
+</style>
---
+++ src/carousel/demos/enUS/transition-name.demo.vue
@@ -0,0 +1,50 @@
+<markdown>
+# Customizing transition
+
+If you want to customize the transition effect, you can use `transition-props` and set `effect` to `'custom'`. For transition props, see [reference](https://v3.vuejs.org/api/built-in-components.html#transition).
+</markdown>
+
+<template>
+  <n-carousel
+    effect="custom"
+    :transition-props="{ name: 'creative' }"
+    show-arrow
+    style="width: 100%; height: 240px"
+  >
+    <img
+      class="carousel-img"
+      src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg"
+    >
+    <img
+      class="carousel-img"
+      src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg"
+    >
+    <img
+      class="carousel-img"
+      src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg"
+    >
+    <img
+      class="carousel-img"
+      src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg"
+    >
+  </n-carousel>
+</template>
+
+<style>
+.carousel-img {
+  width: 100%;
+  height: 240px;
+  object-fit: cover;
+}
+
+:deep(.creative-enter-from),
+:deep(.creative-leave-to) {
+  opacity: 0;
+  transform: scale(0.9);
+}
+
+:deep(.creative-enter-active),
+:deep(.creative-leave-active) {
+  transition: all 0.3s ease;
+}
+</style>
