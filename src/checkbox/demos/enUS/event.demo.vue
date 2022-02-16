74e8934d06373eabaee4f581ddfc128cc9977658
不见月 <61452855+nooooooom@users.noreply.github.com>
2022-02-17 00:04:53 +0800

    docs(checkbox): md to vue (#2421)
    * docs(checkbox): md to vue
    
    * fix(checkbox): fix type

---
+++ src/checkbox/demos/enUS/event.demo.vue
@@ -0,0 +1,46 @@
+<markdown>
+# Event
+</markdown>
+
+<template>
+  <n-space item-style="display: flex;" vertical>
+    <n-checkbox
+      v-model:checked="checked"
+      label="Event"
+      @update:checked="handleCheckedChange"
+    />
+    <n-checkbox-group v-model:value="cities" @update:value="handleUpdateValue">
+      <n-space item-style="display: flex;" align="center">
+        <n-checkbox value="Beijing" label="Beijing" />
+        <n-checkbox value="Shanghai" label="Shanghai" />
+        <n-checkbox value="Guangzhou" label="Guangzhou" />
+        <n-checkbox value="Shenzen" label="Shenzhen" />
+      </n-space>
+    </n-checkbox-group>
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useMessage } from 'naive-ui'
+
+export default defineComponent({
+  setup () {
+    const checkedRef = ref(false)
+    const citiesRef = ref<(string | number)[] | null>(null)
+    const message = useMessage()
+    return {
+      checked: checkedRef,
+      cities: citiesRef,
+      handleCheckedChange (checked: boolean) {
+        checkedRef.value = checked
+        message.info(JSON.stringify(checked))
+      },
+      handleUpdateValue (value: (string | number)[]) {
+        citiesRef.value = value
+        message.info(JSON.stringify(value))
+      }
+    }
+  }
+})
+</script>
