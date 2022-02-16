c5d7e7fdc69ca6322c20fa646f5079e94b34362f
nooooooom <nooooooom@163.com>
2022-02-16 10:46:39 +0800

    docs(tooltip): md to vue

---
+++ src/tooltip/demos/enUS/event.demo.vue
@@ -0,0 +1,53 @@
+<markdown>
+# Event
+</markdown>
+
+<template>
+  <n-space>
+    <n-tooltip
+      placement="bottom"
+      trigger="hover"
+      @update:show="handleUpdateShow"
+    >
+      <template #trigger>
+        <n-button> Hover </n-button>
+      </template>
+      <span> I wish they all could be California girls </span>
+    </n-tooltip>
+    <n-tooltip
+      placement="bottom"
+      trigger="click"
+      @update:show="handleUpdateShow"
+    >
+      <template #trigger>
+        <n-button> Click </n-button>
+      </template>
+      <span> I wish they all could be California girls </span>
+    </n-tooltip>
+    <n-tooltip :show="showPopover" trigger="manual" placement="bottom">
+      <template #trigger>
+        <n-button @click="showPopover = !showPopover">
+          Manual (No event will be emitted)
+        </n-button>
+      </template>
+      <span> I wish they all could be California girls </span>
+    </n-tooltip>
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useMessage } from 'naive-ui'
+
+export default defineComponent({
+  setup () {
+    const message = useMessage()
+    return {
+      showPopover: ref(false),
+      handleUpdateShow (show: boolean) {
+        message.success(show + '')
+      }
+    }
+  }
+})
+</script>
