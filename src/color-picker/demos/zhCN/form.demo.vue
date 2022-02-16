9665ba682a48a89125b426b953775b635e24b630
不见月 <61452855+nooooooom@users.noreply.github.com>
2022-02-16 12:53:11 +0800

    docs(color-picker): md to vue (#2409)

--- src/color-picker/demos/zhCN/form.demo.vue
+++ src/color-picker/demos/zhCN/form.demo.vue
@@ -1,16 +1,18 @@
+<markdown>
 # 和表单一起使用
 
 我感觉这个例子没啥用，不过既然它也算个数据录入组件，就写上吧。
+</markdown>
 
-```html
-<n-form :model="model">
-  <n-form-item label="颜色（#18A058）" path="color" :rule="colorRule">
-    <n-color-picker v-model:value="model.color" :show-alpha="false" />
-  </n-form-item>
-</n-form>
-```
+<template>
+  <n-form :model="model">
+    <n-form-item label="颜色（#18A058）" path="color" :rule="colorRule">
+      <n-color-picker v-model:value="model.color" :show-alpha="false" />
+    </n-form-item>
+  </n-form>
+</template>
 
-```js
+<script lang="ts">
 import { defineComponent, reactive } from 'vue'
 
 export default defineComponent({
@@ -29,4 +31,4 @@ export default defineComponent({
     }
   }
 })
-```
+</script>
---
+++ src/color-picker/demos/zhCN/swatches.demo.vue
@@ -0,0 +1,17 @@
+<markdown>
+# 色板
+
+你也可以预设一个色板供用户选择。
+</markdown>
+
+<template>
+  <n-color-picker
+    :swatches="[
+      '#FFFFFF',
+      '#18A058',
+      '#2080F0',
+      '#F0A020',
+      'rgba(208, 48, 80, 1)'
+    ]"
+  />
+</template>
