03ab6fcb23331efd1795b51902274c7f4a0f9098
不见月 <61452855+nooooooom@users.noreply.github.com>
2022-02-17 00:03:18 +0800

    docs(descriptions): md to vue (#2426)

---
+++ src/descriptions/demos/zhCN/bordered.demo.vue
@@ -0,0 +1,56 @@
+<markdown>
+# 边框
+
+如果有很多多行的信息，你可以把它设为 `bordered`。
+</markdown>
+
+<template>
+  <n-space vertical :size="12">
+    <n-descriptions bordered>
+      <n-descriptions-item>
+        <template #label>
+          早餐
+        </template>
+        苹果
+      </n-descriptions-item>
+      <n-descriptions-item label="午餐">
+        苹果
+      </n-descriptions-item>
+      <n-descriptions-item label="晚餐">
+        苹果
+      </n-descriptions-item>
+      <n-descriptions-item label="为什么长">
+        为什么<br>长<br>长<br>长<br>长<br>长
+      </n-descriptions-item>
+      <n-descriptions-item label="为什么长">
+        为什么<br>长<br>长<br>长<br>长<br>长
+      </n-descriptions-item>
+      <n-descriptions-item label="为什么长">
+        为什么<br>长<br>长<br>长<br>长<br>长
+      </n-descriptions-item>
+    </n-descriptions>
+    <n-descriptions label-placement="left" bordered>
+      <n-descriptions-item>
+        <template #label>
+          Breakfast
+        </template>
+        苹果
+      </n-descriptions-item>
+      <n-descriptions-item label="午餐">
+        苹果
+      </n-descriptions-item>
+      <n-descriptions-item label="晚餐">
+        苹果
+      </n-descriptions-item>
+      <n-descriptions-item label="为什么长">
+        为什么<br>长<br>长<br>长<br>长<br>长
+      </n-descriptions-item>
+      <n-descriptions-item label="为什么长">
+        为什么<br>长<br>长<br>长<br>长<br>长
+      </n-descriptions-item>
+      <n-descriptions-item label="为什么长">
+        为什么<br>长<br>长<br>长<br>长<br>长
+      </n-descriptions-item>
+    </n-descriptions>
+  </n-space>
+</template>
---
+++ src/descriptions/demos/zhCN/placement.demo.vue
@@ -0,0 +1,28 @@
+<markdown>
+# 标签位置
+
+标签位置可以在 `top` 或者 `left`。
+</markdown>
+
+<template>
+  <n-descriptions label-placement="left" title="描述">
+    <n-descriptions-item>
+      <template #label>
+        早餐
+      </template>
+      苹果
+    </n-descriptions-item>
+    <n-descriptions-item label="早午餐">
+      苹果
+    </n-descriptions-item>
+    <n-descriptions-item label="午餐">
+      苹果
+    </n-descriptions-item>
+    <n-descriptions-item label="晚餐">
+      苹果
+    </n-descriptions-item>
+    <n-descriptions-item label="夜宵">
+      苹果
+    </n-descriptions-item>
+  </n-descriptions>
+</template>
