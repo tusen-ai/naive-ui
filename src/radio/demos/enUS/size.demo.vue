ceabe0ef8db07eb3f846b5ef2fcc9dbaf2614cac
nooooooom <nooooooom@163.com>
2022-02-16 13:38:22 +0800

    docs(radio): md to vue

---
+++ src/radio/demos/enUS/size.demo.vue
@@ -0,0 +1,83 @@
+<markdown>
+# Size
+
+Choose the size you want.
+</markdown>
+
+<template>
+  <n-space vertical>
+    <n-radio-group v-model:value="value" name="radiobuttongroup2" size="medium">
+      <n-radio-button
+        v-for="song in songs"
+        :key="song.value"
+        :value="song.value"
+        :disabled="
+          (song.label === 'Live Forever' && disabled1) ||
+            (song.label === 'Shakermaker' && disabled2)
+        "
+      >
+        {{ song.label }}
+      </n-radio-button>
+    </n-radio-group>
+    <n-radio-group v-model:value="value" name="radiobuttongroup3" size="large">
+      <n-radio-button
+        v-for="song in songs"
+        :key="song.value"
+        :value="song.value"
+        :disabled="
+          (song.label === 'Live Forever' && disabled1) ||
+            (song.label === 'Shakermaker' && disabled2)
+        "
+      >
+        {{ song.label }}
+      </n-radio-button>
+    </n-radio-group>
+    <n-space>
+      <n-checkbox v-model:checked="disabled2" style="margin-right: 12px">
+        Disable Shakemaker
+      </n-checkbox>
+      <n-checkbox v-model:checked="disabled1">
+        Disable Live Forever
+      </n-checkbox>
+    </n-space>
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {
+      value: ref(null),
+      disabled2: ref(false),
+      disabled1: ref(false),
+      songs: [
+        {
+          value: "Rock'n'Roll Star",
+          label: "Rock'n'Roll Star"
+        },
+        {
+          value: 'Shakermaker',
+          label: 'Shakermaker'
+        },
+        {
+          value: 'Live Forever',
+          label: 'Live Forever'
+        },
+        {
+          value: 'Up in the Sky',
+          label: 'Up in the Sky'
+        },
+        {
+          value: '...',
+          label: '...'
+        }
+      ].map((s) => {
+        s.value = s.value.toLowerCase()
+        return s
+      })
+    }
+  }
+})
+</script>
---
+++ src/radio/demos/zhCN/radio-focus-debug.demo.vue
@@ -0,0 +1,19 @@
+<markdown>
+# 基础用法
+</markdown>
+
+<template>
+  <n-radio v-model:value-value="value" value="Definitely Maybe">
+    <n-input />
+  </n-radio>
+</template>
+
+<script lang="ts">
+export default {
+  data () {
+    return {
+      value: null
+    }
+  }
+}
+</script>
