bdff3548a82bd529449ae24ed0d557d5b3b7076e
songjianet <1778651752@qq.com>
2022-02-18 15:09:49 +0800

    docs(skeleton): md to vue.

---
+++ src/skeleton/demos/enUS/size.demo.vue
@@ -0,0 +1,37 @@
+<markdown>
+# Size
+
+Use `size` to make its height the same as other components.
+</markdown>
+
+<template>
+  <n-space vertical>
+    <n-space><n-switch v-model:value="loading" />Loading</n-space>
+    <n-space>
+      <n-skeleton v-if="loading" :width="146" :sharp="false" size="medium" />
+      <n-button v-else>
+        Won't you fly high
+      </n-button>
+      <n-skeleton v-if="loading" :width="132" round size="medium" />
+      <n-button v-else round>
+        free bird, yeah
+      </n-button>
+      <n-skeleton v-if="loading" circle size="medium" />
+      <n-button v-else circle>
+        ?
+      </n-button>
+    </n-space>
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {
+      loading: ref(true)
+    }
+  }
+})
+</script>
