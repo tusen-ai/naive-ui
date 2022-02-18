b269782cf68319c5493d1a3e1ec3a28674022b2d
songjianet <1778651752@qq.com>
2022-02-18 10:59:15 +0800

    docs(date-picker): md to vue.

---
+++ src/date-picker/demos/zhCN/update-on-close.demo.vue
@@ -0,0 +1,36 @@
+<markdown>
+# 关闭时更新值
+</markdown>
+
+<template>
+  <n-space vertical>
+    <n-date-picker
+      type="datetime"
+      :default-value="Date.now()"
+      :update-value-on-close="updateValueOnClose"
+    />
+    <n-date-picker
+      :default-value="[Date.now(), Date.now()]"
+      :update-value-on-close="updateValueOnClose"
+      type="daterange"
+    />
+    <n-date-picker
+      :default-value="[Date.now(), Date.now()]"
+      :update-value-on-close="updateValueOnClose"
+      type="datetimerange"
+    />
+    <n-switch v-model:value="updateValueOnClose" />
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {
+      updateValueOnClose: ref(true)
+    }
+  }
+})
+</script>
