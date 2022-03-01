ba2e2f8004e8e32a66148b65a52ee89a0f4f0590
07akioni <07akioni2@gmail.com>
2022-03-02 02:51:49 +0800

    Update src/config-provider/demos/enUS/inline-theme-disabled.demo.vue

--- src/config-provider/demos/enUS/inline-theme-disabled.demo.vue
+++ src/config-provider/demos/enUS/inline-theme-disabled.demo.vue
@@ -3,7 +3,7 @@
 
 By default, naive-ui uses inline style to hold theme variables, so every component is bound with large amount of inline CSS. If you need SSR, or just make your devtools clear, you can enable `inline-theme-disabled` prop.
 
-Note that if you will change `theme-overrides` frequently, it's not recommended to enable the prop. Many useless style elements will be keeped after `theme-overrides` changes.
+Note that if you will change `theme-overrides` frequently, it's not recommended to enable the prop. Many outdated style elements will be keeped after `theme-overrides` changes.
 
 The prop is not responsive.
 </markdown>
