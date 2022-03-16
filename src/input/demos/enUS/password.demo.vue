3bdf8121b31f706095ccd0ebd0a75358cdeebfc5
izcream* <rabbit-dolls@hotmail.com>
2022-03-16 20:56:09 +0700

    docs(input-en): add password toggle icon props document and example

--- src/input/demos/enUS/password.demo.vue
+++ src/input/demos/enUS/password.demo.vue
@@ -3,10 +3,34 @@
 </markdown>
 
 <template>
-  <n-input
-    type="password"
-    show-password-on="mousedown"
-    placeholder="Password"
-    :maxlength="8"
-  />
+  <n-space vertical>
+    <n-input
+      type="password"
+      show-password-on="mousedown"
+      placeholder="Password"
+      :maxlength="8"
+    />
+    <n-input
+      type="password"
+      show-password-on="click"
+      placeholder="Custom Password Toggle Icon"
+      :password-visible-icon="Eye"
+      :password-invisible-icon="EyeOff"
+      :maxlength="8"
+    />
+  </n-space>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Eye, EyeOff } from '@vicons/ionicons5'
+
+export default defineComponent({
+  setup () {
+    return {
+      Eye,
+      EyeOff
+    }
+  }
+})
+</script>
