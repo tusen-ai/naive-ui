0b07fa445693cb4f1c7b6402704b2cf036760660
07akioni <07akioni2@gmail.com>
2022-03-27 21:41:37 +0800

    docs(tabs): fix animated demo overflow problem

--- src/tabs/demos/zhCN/basic.demo.vue
+++ src/tabs/demos/zhCN/basic.demo.vue
@@ -17,7 +17,14 @@
     </n-tabs>
   </n-card>
   <n-card>
-    <n-tabs default-value="signin" size="large" animated>
+    <n-tabs
+      class="card-tabs"
+      default-value="signin"
+      size="large"
+      animated
+      style="margin: 0 -4px"
+      pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;"
+    >
       <n-tab-pane name="signin" tab="登录">
         <n-form>
           <n-form-item-row label="用户名">
@@ -50,3 +57,9 @@
     </n-tabs>
   </n-card>
 </template>
+
+<style>
+.card-tabs .n-tabs-nav--bar-type {
+  padding-left: 4px;
+}
+</style>
