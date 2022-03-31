203654f4cba4b321d959665fe794e56cdef3aea5
XieZongChen <46394163+amadeus711@users.noreply.github.com>
2022-03-31 11:13:56 +0800

    test(icon-wrapper): add test (#2710)

---
+++ src/icon-wrapper/tests/IconWrapper.spec.ts
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils'
+import { NIconWrapper } from '../index'
+
+describe('n-icon', () => {
+  it('should work with import on demand', () => {
+    mount(NIconWrapper)
+  })
+})
