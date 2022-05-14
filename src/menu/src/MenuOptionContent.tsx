6ddce9b90497f5584195e7c8da3dd43fbcdb9c85
吉仔 <33979706+jizai1125@users.noreply.github.com>
2022-05-15 02:54:50 +0800

    fix(menu): render-icon function render incorrect when return true (#2923)

--- src/menu/src/MenuOptionContent.tsx
+++ src/menu/src/MenuOptionContent.tsx
@@ -95,7 +95,7 @@ export default defineComponent({
             style={this.iconStyle}
             role="none"
           >
-            {icon}
+            {[icon]}
           </div>
         )}
         <div class={`${clsPrefix}-menu-item-content-header`} role="none">
