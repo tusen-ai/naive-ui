4c6f5dc8b3c73e19d175c58cd84a11ca0258a2ce
07akioni <07akioni2@gmail.com>
2022-05-16 03:08:38 +0800

    wip: fix build script

--- scripts/post-build/index.js
+++ scripts/post-build/index.js
@@ -13,8 +13,10 @@ const { genWebTypes } = require('./gen-web-types')
   })
   await replaceDefine([srcDir], {
     "'lodash'": "'lodash-es'",
-    "'date-fns(.*)'": "'date-fns/esm$1'",
-    "'date-fns-tz(.*)'": "'date-fns-tz/esm$1'"
+    "'date-fns'": "'date-fns/esm'",
+    "'date-fns-tz'": "'date-fns-tz/esm'",
+    "'date-fns/(.*)'": "'date-fns/esm/$1'",
+    "'date-fns-tz/(.*)'": "'date-fns-tz/esm/$1'"
   })
 
   // generate web-types.json for webstorm & vetur
