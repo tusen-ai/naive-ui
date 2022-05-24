7a20e97162b9b21939a3483765212aad24a21fb3
wegi8 <18272190626@163.com>
2022-05-24 18:51:08 +0800

    docs: update the slider docs for  identity slot

---
+++ src/slider/demos/enUS/custom-identification.demo.vue
@@ -0,0 +1,32 @@
+<markdown>
+# Custom identification
+
+You can use identity slots to customize identification.
+</markdown>
+
+<template>
+  <n-space vertical>
+    <n-slider v-model:value="value" :step="10">
+      <template #identity>
+        <n-icon-wrapper :size="24" :border-radius="10">
+          <n-icon :size="18" :component="AirplaneTakeOff16Regular" />
+        </n-icon-wrapper>
+      </template>
+    </n-slider>
+    <n-input-number v-model:value="value" size="small" />
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import AirplaneTakeOff16Regular from '@vicons/fluent/AirplaneTakeOff16Regular'
+
+export default defineComponent({
+  setup () {
+    return {
+      AirplaneTakeOff16Regular,
+      value: ref(50)
+    }
+  }
+})
+</script>
