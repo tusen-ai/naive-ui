99f375d15186b6cd1fc7f4087cfe87f4153c5999
wegi8 <18272190626@163.com>
2022-05-27 11:13:56 +0800

    chore: migrate demo

---
+++ src/slider/demos/zhCN/custom-identification.demo.vue
@@ -0,0 +1,31 @@
+<markdown>
+# 自定义标识
+
+可以使用identity插槽自定义标识点。
+</markdown>
+
+<template>
+  <n-space vertical>
+    <n-slider v-model:value="value" :step="10">
+      <template #identity>
+        <n-icon-wrapper :size="24" :border-radius="10">
+          <n-icon :size="18" :component="AirplaneTakeOff16Regular" />
+        </n-icon-wrapper>
+      </template>
+    </n-slider>
+    <n-input-number v-model:value="value" size="small" />
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import AirplaneTakeOff16Regular from '@vicons/fluent/AirplaneTakeOff16Regular'
+export default defineComponent({
+  setup () {
+    return {
+      AirplaneTakeOff16Regular,
+      value: ref(50)
+    }
+  }
+})
+</script>
