88fb046dceb7231ab25ac51a4a5c4af6811eca5d
XieZongChen <46394163+amadeus711@users.noreply.github.com>
2022-06-24 00:03:33 +0800

    Update vi.ts

--- src/locales/date/vi.ts
+++ src/locales/date/vi.ts
@@ -1,15 +1,9 @@
 import viVN from 'date-fns/esm/locale/vi'
-import type { Locale } from 'date-fns'
-
-interface NDateLocale {
-  name: string
-  locale: Locale
-}
+import { NDateLocale } from './enUS'
 
 const dateVi: NDateLocale = {
-  name: 'viVN',
+  name: 'vi-VN',
   locale: viVN
 }
 
-export { NDateLocale }
 export default dateVi
