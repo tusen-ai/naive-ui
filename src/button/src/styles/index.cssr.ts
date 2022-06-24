9959622ee85af239f2c6e74e3ef974a8513623ba
thelostword <thelostword@163.com>
2022-06-24 18:12:24 +0800

    fix(button): style

--- src/button/src/styles/index.cssr.ts
+++ src/button/src/styles/index.cssr.ts
@@ -195,11 +195,9 @@ export default c([
       cB('icon-slot', `
         height: var(--n-icon-size);
         width: var(--n-icon-size);
-        position: absolute;
-        left: 0;
-        top: 50%;
-        transform: translateY(-50%);
         display: flex;
+        align-items: center;
+        justify-content: center;
       `, [
         iconSwitchTransition({
           top: '50%',
