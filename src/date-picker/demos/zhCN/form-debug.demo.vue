8c2da9fb1305b21e776075d599667065bca7a7f1
07akioni <07akioni2@gmail.com>
2022-06-29 02:07:23 +0800

    docs(form): number rule caveat

--- src/date-picker/demos/zhCN/form-debug.demo.vue
+++ src/date-picker/demos/zhCN/form-debug.demo.vue
@@ -3,19 +3,17 @@
 </markdown>
 
 <template>
-  <div id="app">
-    <n-form :model="model" :rules="rules">
-      <n-form-item :span="12" label="产检时间" path="date">
-        <n-date-picker
-          v-model:value="model.date"
-          style="width: 100%"
-          type="date"
-          clearable
-        />
-      </n-form-item>
-    </n-form>
-    <pre>{{ model.date }}</pre>
-  </div>
+  <n-form :model="model" :rules="rules">
+    <n-form-item :span="12" label="产检时间" path="date">
+      <n-date-picker
+        v-model:value="model.date"
+        style="width: 100%"
+        type="date"
+        clearable
+      />
+    </n-form-item>
+  </n-form>
+  <pre>{{ model.date }}</pre>
 </template>
 
 <script lang="ts">
--- src/form/demos/zhCN/index.demo-entry.md
+++ src/form/demos/zhCN/index.demo-entry.md
@@ -4,6 +4,10 @@
 
 收集、验证信息。
 
+<n-alert type="warning" title="注意">
+  如果你需要为一个值为 number 类型的表项设定 required，你需要在 rule 对象中设定 <n-text code>`type: number`</n-text>。
+</n-alert>
+
 ## 演示
 
 ```demo
