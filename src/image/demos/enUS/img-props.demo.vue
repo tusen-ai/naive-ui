2e8d1c1906ced2cee4c54eeae290421690fd494f
Robin Kehl <robin.kehl@singular-it.de>
2022-08-15 20:01:36 +0200

    docs(n-image): add example with `img-props-preview`

--- src/image/demos/enUS/img-props.demo.vue
+++ src/image/demos/enUS/img-props.demo.vue
@@ -8,8 +8,7 @@ You can set e.g. `aspect-ratio` via `:img-props`
   <n-image
     width="100"
     src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
-    :img-props="{
-      style: { 'aspect-ratio': '16 / 9' }
-    }"
+    :img-props="{ style: { 'aspect-ratio': '16 / 9' } }"
+    :img-props-preview="{ style: { 'aspect-ratio': '9 / 16' } }"
   />
 </template>
