b4c08778fb682ae33051268b5499c5255ffe0899
07akioni <07akioni2@gmail.com>
2022-08-17 00:04:25 +0800

    fix(select): export NodeProps

--- src/data-table/demos/enUS/custom-expand-icon.demo.vue
+++ src/data-table/demos/enUS/custom-expand-icon.demo.vue
@@ -1,18 +1,15 @@
 <markdown>
 # Customize expand icon
-
 </markdown>
 
 <template>
   <n-data-table :columns="columns" :data="data" default-expand-all />
-  <br>
-  <n-data-table :columns="columns1" :data="data1" default-expand-all />
 </template>
 
 <script lang="ts">
 import { h, defineComponent } from 'vue'
 import type { DataTableColumns } from 'naive-ui'
-import { ArrowForward, ArrowForwardCircleOutline } from '@vicons/ionicons5'
+import { ArrowForward } from '@vicons/ionicons5'
 
 type RowData = {
   name: string
@@ -64,59 +61,11 @@ const columns: DataTableColumns<RowData> = [
   }
 ]
 
-const columns1: DataTableColumns<RowData> = [
-  {
-    type: 'selection'
-  },
-  {
-    title: 'name',
-    key: 'name',
-    renderExpandIcon: (expanded) => {
-      return h(ArrowForwardCircleOutline, {
-        style: expanded ? 'transform: rotate(90deg);' : undefined
-      })
-    }
-  },
-  {
-    title: 'index',
-    key: 'key'
-  }
-]
-
-const data1: RowData[] = [
-  {
-    name: '07akioni',
-    key: '07',
-    children: [
-      {
-        name: '08akioni',
-        key: '08',
-        children: [
-          {
-            name: '09akioni',
-            key: '09'
-          },
-          {
-            name: '10akioni',
-            key: '10'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: '11akioni',
-    key: '11'
-  }
-]
-
 export default defineComponent({
   setup () {
     return {
       data: createData(),
-      columns,
-      columns1,
-      data1
+      columns
     }
   }
 })
