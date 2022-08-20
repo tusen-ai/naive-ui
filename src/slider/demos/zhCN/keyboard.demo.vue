64ea50bf612c45232b32a64ecad2b99aa634238e
Sepush <sepush@outlook.com>
2022-08-21 00:11:12 +0800

    feat(slider): add `keyboard` prop (#3560)
    Signed-off-by: Sepush <sepush@outlook.com>
    
    Signed-off-by: Sepush <sepush@outlook.com>
    Co-authored-by: 07akioni <07akioni2@gmail.com>

---
+++ src/slider/demos/zhCN/keyboard.demo.vue
@@ -0,0 +1,24 @@
+<markdown>
+# 键盘事件
+</markdown>
+
+<template>
+  <n-space vertical>
+    <n-switch v-model:value="keyboard" />
+    <n-slider v-model:value="value" :step="10" :keyboard="keyboard" />
+    <n-input-number v-model:value="value" size="small" />
+  </n-space>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {
+      value: ref(50),
+      keyboard: ref(false)
+    }
+  }
+})
+</script>
