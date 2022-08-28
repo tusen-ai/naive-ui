a491f501b00e2b91cc355a0b422431f18c0448b1
OrbisK <37191683+OrbisK@users.noreply.github.com>
2022-08-28 11:15:49 +0200

    refactor(mention): cn translation
    Co-authored-by: XieZongChen <46394163+amadeus711@users.noreply.github.com>

--- src/mention/demos/enUS/input-props.demo.vue
+++ src/mention/demos/enUS/input-props.demo.vue
@@ -2,7 +2,7 @@
 # Input props
 
 <n-alert type="warning">
-Props and defaults from <n-code>n-mention</n-code> will override props set with <n-code>input-props</n-code>. To set <n-code>placeholder</n-code> or <n-code>disabled</n-code>, set it directly on <n-code>n-mention</n-code>.
+Props and defaults from `n-mention` will override props set with `input-props`. To set `placeholder` or `disabled`, set it directly on `n-mention`.
 </n-alert>
 </markdown>
 
