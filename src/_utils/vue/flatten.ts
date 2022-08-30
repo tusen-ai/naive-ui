6950d88b6b72897312063fd26eba3681d7ee63c0
wanchun <445436867@qq.com>
2022-08-30 20:35:23 +0800

    style: 格式化代码

--- src/_utils/vue/flatten.ts
+++ src/_utils/vue/flatten.ts
@@ -1,38 +1,38 @@
-import { Fragment, VNodeChild, createTextVNode, VNode, Comment } from 'vue'
+import { Fragment, createTextVNode, VNode } from 'vue'
 
 // o(n) flatten
-export function flatten(vNodes: VNode[], result: VNode[] = [], key?: string) {
+export function flatten (
+  vNodes: VNode[],
+  result: VNode[] = [],
+  key?: string
+): VNode[] {
   vNodes.forEach((vNode) => {
-    if (vNode === null) return;
+    if (vNode === null) return
     if (typeof vNode !== 'object') {
       if (typeof vNode === 'string' || typeof vNode === 'number') {
-          result.push(createTextVNode(String(vNode)));
+        result.push(createTextVNode(String(vNode)))
       }
-      return;
+      return
     }
     if (Array.isArray(vNode)) {
-      flatten(vNode, result);
-      return;
+      flatten(vNode, result)
+      return
     }
     if (vNode.type === Fragment) {
-      if (vNode.children === null) return;
-      const currentKey = key
-        ? `${key}_${String(vNode.key)}`
-        : String(vNode.key);
+      if (vNode.children === null) return
+      const currentKey = key ? `${key}_${String(vNode.key)}` : String(vNode.key)
       if (Array.isArray(vNode.children)) {
-        (vNode.children as VNode[]).forEach(
-          (node: VNode, index: number) => {
-            if (node.key === undefined || node.key === null) {
-              node.key = `${currentKey}_${index}`;
-            }
-          },
-        );
-        flatten(vNode.children as VNode[], result, currentKey);
+        ;(vNode.children as VNode[]).forEach((node: VNode, index: number) => {
+          if (node.key === undefined || node.key === null) {
+            node.key = `${currentKey}_${index}`
+          }
+        })
+        flatten(vNode.children as VNode[], result, currentKey)
       }
       // rawSlot
     } else {
-      result.push(vNode);
+      result.push(vNode)
     }
-  });
-  return result;
+  })
+  return result
 }
