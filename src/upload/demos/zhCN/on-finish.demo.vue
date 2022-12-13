6767db54f56582f49d7c239a56f29c3ff3ac8b64
Adang <lundang.liu@feima-inc.com>
2022-12-13 14:06:04 +0800

    增加 keep-file-after-finish 属性允许用户配置在on-finish 中保留 file
    关闭 #3868

--- src/upload/demos/zhCN/on-finish.demo.vue
+++ src/upload/demos/zhCN/on-finish.demo.vue
@@ -1,12 +1,14 @@
 <markdown>
 # 上传完成的回调
 
-你可以在回调中修改文件的属性。
+你可以在回调中修改文件的属性。使用`keep-file-after-finish`允许在回调中返回File。
 </markdown>
 
 <template>
+  KeepFileAfterFinish <n-switch v-model:value="keepFileAfterFinish" /><br>
   <n-upload
     action="__HTTP__://www.mocky.io/v2/5e4bafc63100007100d8b70f"
+    :keep-file-after-finish="keepFileAfterFinish"
     @finish="handleFinish"
   >
     <n-button>上传文件</n-button>
@@ -14,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useMessage } from 'naive-ui'
 import type { UploadFileInfo } from 'naive-ui'
 
@@ -29,6 +31,7 @@ export default defineComponent({
       event?: ProgressEvent
     }) => {
       console.log(event)
+      console.log(file.file)
       message.success((event?.target as XMLHttpRequest).response)
       const ext = file.name.split('.')[1]
       file.name = `更名.${ext}`
@@ -36,6 +39,7 @@ export default defineComponent({
       return file
     }
     return {
+      keepFileAfterFinish: ref(false),
       message,
       handleFinish
     }
