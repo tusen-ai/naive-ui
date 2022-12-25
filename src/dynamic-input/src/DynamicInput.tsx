7dfe9bfd32f35ccd9bb7d5032e4fed09cfa67228
Silas <42947316+hedwiggggg@users.noreply.github.com>
2022-12-25 07:29:09 +0100

    fix(dynamic-input): wrong index on create click (#3816)
    When the Create button is pressed on DynamicInput, the onCreate handler is called with the wrong index.

--- src/dynamic-input/src/DynamicInput.tsx
+++ src/dynamic-input/src/DynamicInput.tsx
@@ -186,7 +186,7 @@ export default defineComponent({
       doUpdateValue(newValue)
     }
     function handleCreateClick (): void {
-      createItem(0)
+      createItem(-1)
     }
     function createItem (index: number): void {
       const { value: mergedValue } = mergedValueRef
