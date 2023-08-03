28518508ff7c39cc1dcffb1e02a1e91d9a048514
XieZongChen <46394163+amadeus711@users.noreply.github.com>
2023-08-03 09:35:31 +0800

    feat(input): optimize `on-keyup` (#5126)
    * feat(input): optimize on-keyup
    
    * feat(input): optimize on-keyup

--- src/input/src/Input.tsx
+++ src/input/src/Input.tsx
@@ -174,7 +174,6 @@ export default defineComponent({
   name: 'Input',
   props: inputProps,
   setup (props) {
-    console.log(props)
     if (__DEV__) {
       watchEffect(() => {
         if (props.showPasswordToggle) {
@@ -659,6 +658,9 @@ export default defineComponent({
       }
       on('mouseup', document, hidePassword)
     }
+    function handleWrapperKeyup (e: KeyboardEvent): void {
+      if (props.onKeyup) call(props.onKeyup, e)
+    }
     function handleWrapperKeydown (e: KeyboardEvent): void {
       props.onKeydown?.(e)
       switch (e.key) {
@@ -989,6 +991,7 @@ export default defineComponent({
       handlePasswordToggleClick,
       handlePasswordToggleMousedown,
       handleWrapperKeydown,
+      handleWrapperKeyup,
       handleTextAreaMirrorResize,
       getTextareaScrollContainer: () => {
         return textareaElRef.value
@@ -1054,7 +1057,7 @@ export default defineComponent({
         onMouseleave={this.handleMouseLeave}
         onCompositionstart={this.handleCompositionStart}
         onCompositionend={this.handleCompositionEnd}
-        onKeyup={this.onKeyup}
+        onKeyup={this.handleWrapperKeyup}
         onKeydown={this.handleWrapperKeydown}
       >
         {/* textarea & basic input */}
