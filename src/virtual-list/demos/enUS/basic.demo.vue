3791e3f7b5c9bd892375393c9ddbb36e8c80dc20
jahnli <jahnli@163.com>
2023-09-23 00:56:45 +0800

    feat: add avatar list

--- src/virtual-list/demos/enUS/basic.demo.vue
+++ src/virtual-list/demos/enUS/basic.demo.vue
@@ -1,11 +1,12 @@
 <markdown>
-  # Basic
+  # Basic size
 </markdown>
 
 <template>
-  <n-virtual-list style="max-height: 240px" :item-size="23" :items="basicData">
+  <n-virtual-list style="max-height: 240px" :item-size="42" :items="items">
     <template #default="{ item }">
-      <div :key="item.key" style="height: 23px">
+      <div :key="item.key" class="item" style="height: 42px">
+        <img class="avatar" :src="item.avatar" alt="">
         <span> {{ item.value }}</span>
       </div>
     </template>
@@ -16,6 +17,14 @@
 import { defineComponent } from 'vue'
 export default defineComponent({
   setup () {
+    const avatars = [
+      'https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg',
+      'https://avatars.githubusercontent.com/u/20943608?s=60&v=4',
+      'https://avatars.githubusercontent.com/u/46394163?s=60&v=4',
+      'https://avatars.githubusercontent.com/u/39197136?s=60&v=4',
+      'https://avatars.githubusercontent.com/u/19239641?s=60&v=4'
+    ]
+
     function createArray (length: number): number[] {
       const ret = []
       for (let i = 0; i < length; ++i) {
@@ -24,14 +33,27 @@ export default defineComponent({
       return ret
     }
 
-    const basicData = createArray(10000).map((_, i) => ({
+    const items = createArray(10000).map((_, i) => ({
       key: i,
-      value: `item-${i}`
+      value: i,
+      avatar: avatars[i % avatars.length]
     }))
 
     return {
-      basicData
+      items
     }
   }
 })
 </script>
+<style>
+.item {
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+.avatar {
+  width: 28px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+</style>
---
+++ src/virtual-list/demos/enUS/dynamic-size.vue
@@ -0,0 +1,37 @@
+<markdown>
+  # Basic size.
+</markdown>
+
+<template>
+  <n-virtual-list style="max-height: 240px" :item-size="23" :items="basicData">
+    <template #default="{ item }">
+      <div :key="item.key" style="height: 23px">
+        <span> {{ item.value }}</span>
+      </div>
+    </template>
+  </n-virtual-list>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup () {
+    function createArray (length: number): number[] {
+      const ret = []
+      for (let i = 0; i < length; ++i) {
+        ret.push(i)
+      }
+      return ret
+    }
+
+    const basicData = createArray(10000).map((_, i) => ({
+      key: i,
+      value: i
+    }))
+
+    return {
+      basicData
+    }
+  }
+})
+</script>
