0de95db645cdde35059db2ee633333ae4d6a5fa2
OrbisK <37191683+OrbisK@users.noreply.github.com>
2023-12-20 10:01:14 +0000

    feat(input-number): add `input-props` prop
    closes #5450

--- src/input-number/src/InputNumber.tsx
+++ src/input-number/src/InputNumber.tsx
@@ -8,7 +8,8 @@ import {
   type PropType,
   watchEffect,
   type VNode,
-  nextTick
+  nextTick,
+  type InputHTMLAttributes
 } from 'vue'
 import { rgba } from 'seemly'
 import { useMemo, useMergedState } from 'vooks'
@@ -76,6 +77,7 @@ export const inputNumberProps = {
     type: String as PropType<'right' | 'both'>,
     default: 'right'
   },
+  inputProps: Object as PropType<InputHTMLAttributes>,
   readonly: Boolean,
   clearable: Boolean,
   keyboard: {
@@ -692,6 +694,7 @@ export default defineComponent({
           onMousedown={this.handleMouseDown}
           onClear={this.handleClear}
           clearable={this.clearable}
+          inputProps={this.inputProps}
           internalLoadingBeforeSuffix
         >
           {{
