855e919caa4a2de6c71417fa973a298f1a11e0ea
snow <1006084794@qq.com>
2024-01-11 12:01:19 +0800

    feat(split): add size and on-update:size, controlled properties

--- src/split/demos/enUS/controlled.demo.vue
+++ src/split/demos/enUS/controlled.demo.vue
@@ -4,7 +4,7 @@
 
 <template>
   <n-input-number v-model:value="split" :step="0.1" clearable />
-  <NSplit v-model:size="split" style="height: 100%">
+  <NSplit :size="split" style="height: 100%">
     <template #1>
       <div style="width: 100%; background-color: black" />
     </template>
