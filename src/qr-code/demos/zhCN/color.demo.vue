2fd533c7e45a6e6ac9820dc2d7531f0f8bc85085
Sepush <sepush@outlook.com>
2024-01-28 17:33:46 +0800

    fix(qr-code): style of size
    Signed-off-by: Sepush <sepush@outlook.com>

--- src/qr-code/demos/zhCN/color.demo.vue
+++ src/qr-code/demos/zhCN/color.demo.vue
@@ -1,16 +1,37 @@
 <markdown>
   # 颜色
 
-让二维码不再单调乏味。
-  </markdown>
+  让二维码不再单调乏味。
+</markdown>
 
 <template>
-  <n-space>
+  <n-flex>
     <n-qr-code value="https://www.naiveui.com/" color="#18a058" />
     <n-qr-code
       value="https://www.naiveui.com/"
       color="#409eff"
       background-color="#F5F5F5"
     />
-  </n-space>
+  </n-flex>
+  <n-flex vertical>
+    <n-color-picker v-model:value="color" />
+    <n-qr-code
+      value="https://www.naiveui.com/"
+      :color="color"
+      background-color="#F5F5F5"
+    />
+  </n-flex>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const color = ref('#225A95FF')
+    return {
+      color
+    }
+  }
+})
+</script>
