e26ef06cca49e0b43b0d5c7e93154621fbacbc28
xiyueW <xiximeili@sina.com>
2024-05-13 15:05:18 +0800

    Update Menu.tsx
    当menu添加responsive后，其上的属性无法透传至正确的位置

--- src/menu/src/Menu.tsx
+++ src/menu/src/Menu.tsx
@@ -10,7 +10,8 @@ import {
   inject,
   type VNodeChild,
   watchEffect,
-  type VNode
+  type VNode,
+  mergeProps
 } from 'vue'
 import { createTreeMate, type Key } from 'treemate'
 import { useCompitable, useMergedState } from 'vooks'
@@ -620,16 +621,17 @@ export default defineComponent({
     const horizontal = mode === 'horizontal'
     const finalResponsive = horizontal && this.responsive
     const renderMainNode = (): VNode => (
-      <div
-        role={mode === 'horizontal' ? 'menubar' : 'menu'}
-        class={[
-          `${mergedClsPrefix}-menu`,
-          themeClass,
-          `${mergedClsPrefix}-menu--${mode}`,
-          finalResponsive && `${mergedClsPrefix}-menu--responsive`,
-          this.mergedCollapsed && `${mergedClsPrefix}-menu--collapsed`
-        ]}
-        style={this.cssVars}
+      <div {...mergeProps(this.$attrs, {
+          role: mode === 'horizontal' ? 'menubar' : 'menu',
+          class: [
+            `${mergedClsPrefix}-menu`,
+            themeClass,
+            `${mergedClsPrefix}-menu--${mode}`,
+            finalResponsive && `${mergedClsPrefix}-menu--responsive`,
+            this.mergedCollapsed && `${mergedClsPrefix}-menu--collapsed`
+          ],
+          style: this.cssVars
+        })}
       >
         {finalResponsive ? (
           <VOverflow
