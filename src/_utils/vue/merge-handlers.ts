1313ce31a5202c2ed5b403f40f5431e8237b1eaa
07akioni <07akioni2@gmail.com>
2024-07-11 22:54:07 +0800

    fix: eslint

--- src/_utils/vue/merge-handlers.ts
+++ src/_utils/vue/merge-handlers.ts
@@ -4,7 +4,7 @@ export function mergeEventHandlers<T> (
   const filteredHandlers = handlers.filter((handler) => handler !== undefined)
   if (filteredHandlers.length === 0) return undefined
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  if (filteredHandlers.length === 1) return filteredHandlers[0]!
+  if (filteredHandlers.length === 1) return filteredHandlers[0]
   return (e) => {
     handlers.forEach((handler) => {
       if (handler) {
