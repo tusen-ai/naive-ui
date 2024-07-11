10f515f8f661c19059927de9c5179d37d8d25d83
07akioni <07akioni2@gmail.com>
2024-07-12 01:03:34 +0800

    fix: type

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,27 +9,28 @@ export default antfu(
       'test/unit/coverage',
       'src/_deprecated/icons',
       'dist',
-      'es',
-    ],
+      'es'
+    ]
   },
   {
     files: ['**/*.demo.vue'],
     rules: {
       'no-console': 'off',
-      'vue/one-component-per-file': 'off',
-    },
+      'vue/one-component-per-file': 'off'
+    }
   },
   {
     files: ['**/*.tsx'],
     rules: {
-      'unused-imports/no-unused-imports': 'off',
-    },
+      'unused-imports/no-unused-imports': 'off'
+    }
   },
   {
     files: ['**/*'],
     rules: {
       'style/multiline-ternary': 'off',
       'style/max-statements-per-line': 'off',
-    },
-  },
+      'style/comma-dangle': 'off'
+    }
+  }
 )
