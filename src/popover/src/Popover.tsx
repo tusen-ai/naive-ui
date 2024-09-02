3984b0a0e2ec31c3cb55d43a263d6509f1b25529
jizai1125 <1414589221@qq.com>
2024-09-02 10:46:00 +0800

    fix(popover): `PopoverProps` type to include `theme-overrides`, closes #6292

--- src/popover/src/Popover.tsx
+++ src/popover/src/Popover.tsx
@@ -219,7 +219,7 @@ export const popoverProps = {
   internalRenderBody: Function as PropType<InternalRenderBody>
 }
 
-export type PopoverProps = ExtractPublicPropTypes<typeof popoverBaseProps>
+export type PopoverProps = ExtractPublicPropTypes<typeof popoverProps>
 export type PopoverInternalProps = ExtractInternalPropTypes<typeof popoverProps>
 
 export default defineComponent({
