31b342a075c71a25163a294cb1635b66936c8e2a
zhengchangfu <zcfpromise@163.com>
2024-09-04 15:42:58 +0800

    fix(replaceable): 修复 replaceable 因为闭包导致的 dom 内存泄漏问题

--- src/_internal/icons/Retry.tsx
+++ src/_internal/icons/Retry.tsx
@@ -1,16 +1,31 @@
-import { h } from 'vue'
-import { replaceable } from './replaceable'
+import { defineComponent, h, inject } from 'vue'
+import { upperFirst } from 'lodash-es'
+import { configProviderInjectionKey } from '../../config-provider/src/context'
 
-export default replaceable(
-  'retry',
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-    <path
-      d="M320,146s24.36-12-64-12A160,160,0,1,0,416,294"
-      style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-miterlimit: 10; stroke-width: 32px;"
-    />
-    <polyline
-      points="256 58 336 138 256 218"
-      style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
-    />
-  </svg>
-)
+const name = 'retry'
+export default defineComponent({
+  name: upperFirst(name),
+  setup() {
+    const mergedIconsRef = inject(
+      configProviderInjectionKey,
+      null
+    )?.mergedIconsRef
+    return () => {
+      const iconOverride = mergedIconsRef?.value?.[name]
+      return iconOverride ? (
+        iconOverride()
+      ) : (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path
+            d="M320,146s24.36-12-64-12A160,160,0,1,0,416,294"
+            style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-miterlimit: 10; stroke-width: 32px;"
+          />
+          <polyline
+            points="256 58 336 138 256 218"
+            style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
+          />
+        </svg>
+      )
+    }
+  }
+})
--- src/_internal/icons/Trash.tsx
+++ src/_internal/icons/Trash.tsx
@@ -1,39 +1,54 @@
-import { h } from 'vue'
-import { replaceable } from './replaceable'
+import { defineComponent, h, inject } from 'vue'
+import { upperFirst } from 'lodash-es'
+import { configProviderInjectionKey } from '../../config-provider/src/context'
 
-export default replaceable(
-  'trash',
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-    <path
-      d="M432,144,403.33,419.74A32,32,0,0,1,371.55,448H140.46a32,32,0,0,1-31.78-28.26L80,144"
-      style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
-    >
-    </path>
-    <rect
-      x="32"
-      y="64"
-      width="448"
-      height="80"
-      rx="16"
-      ry="16"
-      style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
-    >
-    </rect>
-    <line
-      x1="312"
-      y1="240"
-      x2="200"
-      y2="352"
-      style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
-    >
-    </line>
-    <line
-      x1="312"
-      y1="352"
-      x2="200"
-      y2="240"
-      style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
-    >
-    </line>
-  </svg>
-)
+const name = 'trash'
+export default defineComponent({
+  name: upperFirst(name),
+  setup() {
+    const mergedIconsRef = inject(
+      configProviderInjectionKey,
+      null
+    )?.mergedIconsRef
+    return () => {
+      const iconOverride = mergedIconsRef?.value?.[name]
+      return iconOverride ? (
+        iconOverride()
+      ) : (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path
+            d="M432,144,403.33,419.74A32,32,0,0,1,371.55,448H140.46a32,32,0,0,1-31.78-28.26L80,144"
+            style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
+          >
+          </path>
+          <rect
+            x="32"
+            y="64"
+            width="448"
+            height="80"
+            rx="16"
+            ry="16"
+            style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
+          >
+          </rect>
+          <line
+            x1="312"
+            y1="240"
+            x2="200"
+            y2="352"
+            style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
+          >
+          </line>
+          <line
+            x1="312"
+            y1="352"
+            x2="200"
+            y2="240"
+            style="fill: none; stroke: currentcolor; stroke-linecap: round; stroke-linejoin: round; stroke-width: 32px;"
+          >
+          </line>
+        </svg>
+      )
+    }
+  }
+})
