800dc3b755f43393634678c04ec432ec8dfa9528
jizai1125 <1414589221@qq.com>
2024-09-09 12:30:44 +0800

    feat(infinite): add `reverse` prop, closes #6308

--- src/infinite-scroll/demos/zhCN/basic.demo.vue
+++ src/infinite-scroll/demos/zhCN/basic.demo.vue
@@ -2,27 +2,37 @@
 # 基础
 </markdown>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue'
 
-export default defineComponent({
-  setup() {
-    const count = ref(6)
-    const handleLoad = () => {
-      count.value += 1
-    }
-    return {
-      count,
-      handleLoad
-    }
+const dataRef = ref<number[]>([])
+const loading = ref(false)
+async function handleLoad() {
+  loading.value = true
+  try {
+    const data = await loadData()
+    dataRef.value.push(...data)
   }
-})
+  finally {
+    loading.value = false
+  }
+}
+function loadData(): Promise<number[]> {
+  const index = dataRef.value.length
+  const result = Array.from({ length: 10 })
+    .fill(null)
+    .map((_, i) => index + i + 1)
+  return new Promise(resolve => setTimeout(() => resolve(result), 1000))
+}
 </script>
 
 <template>
-  <n-infinite-scroll style="height: 240px" :distance="10" @load="handleLoad">
-    <div v-for="i in count" :key="i" class="item">
+  <n-infinite-scroll style="height: 240px" :distance="100" @load="handleLoad">
+    <div v-for="i in dataRef" :key="i" class="item">
       {{ i }}
+    </div>
+    <div v-if="loading" class="text">
+      loading...
     </div>
   </n-infinite-scroll>
 </template>
@@ -36,8 +46,10 @@ export default defineComponent({
   margin-bottom: 10px;
   background-color: rgba(0, 128, 0, 0.16);
 }
-
 .item:last-child {
   margin-bottom: 0;
+}
+.text {
+  text-align: center;
 }
 </style>
