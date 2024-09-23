78b13eccf504ac3afff40664cfc1fe1a4121e98d
jahnli <jahnli@163.com>
2024-09-24 00:26:20 +0800

    feat: add demo

---
+++ src/qr-code/demos/zhCN/shape.demo.vue
@@ -0,0 +1,29 @@
+<markdown>
+# 形状
+</markdown>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const text = ref('雨淋湿了天空')
+    return {
+      text
+    }
+  }
+})
+</script>
+
+<template>
+  <n-space vertical>
+    <n-flex>
+      <n-qr-code :value="text" />
+      <n-qr-code :value="text" shape="circle" />
+      <n-qr-code :value="text" shape="star" />
+      <n-qr-code :value="text" shape="heart" />
+      <n-qr-code :value="text" shape="cylinder" />
+    </n-flex>
+    <n-input v-model:value="text" :maxlength="60" type="text" />
+  </n-space>
+</template>
