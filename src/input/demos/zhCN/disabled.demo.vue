b8d859cbc6a7ba71d8c0f9e3a09e960279323ffe
jizai1125 <1414589221@qq.com>
2024-09-27 15:36:46 +0800

    fix(input): `readonly` prop should only be Boolean due to Vue casting rule

--- src/input/demos/zhCN/disabled.demo.vue
+++ src/input/demos/zhCN/disabled.demo.vue
@@ -1,5 +1,5 @@
 <markdown>
-# 禁用
+# 禁用 & 只读
 
 文本输入可以被禁用。
 </markdown>
@@ -12,7 +12,8 @@ export default defineComponent({
   setup() {
     return {
       FlashOutline,
-      active: ref(false)
+      disabled: ref(false),
+      readonly: ref(false)
     }
   }
 })
@@ -24,21 +25,35 @@ export default defineComponent({
       type="text"
       size="small"
       placeholder="噢！它被禁用了。"
-      :disabled="!active"
       round
+      clearable
+      :disabled="disabled"
+      :readonly="readonly"
     />
     <n-input
       type="textarea"
       size="small"
       placeholder="噢！它被禁用了。"
-      :disabled="!active"
       round
+      clearable
+      :disabled="disabled"
+      :readonly="readonly"
     />
-    <n-input pair separator="to" clearable :disabled="!active">
-      <template #affix>
+    <n-input
+      pair
+      separator="to"
+      :placeholder="['from', 'to']"
+      clearable
+      :disabled="disabled"
+      :readonly="readonly"
+    >
+      <template #prefix>
         <n-icon :component="FlashOutline" />
       </template>
     </n-input>
-    <n-switch v-model:value="active" />
+    <n-flex>
+      <n-flex>禁用 <n-switch v-model:value="disabled" /> </n-flex>
+      <n-flex>只读 <n-switch v-model:value="readonly" /> </n-flex>
+    </n-flex>
   </n-space>
 </template>
