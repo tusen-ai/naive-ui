d674948360406a9698c65f31747af6e2f482ef71
jahnli <jahnli@163.com>
2024-10-01 20:15:11 +0800

    feat: add demo

---
+++ src/progress/demos/enUS/linear-gradient.demo.vue
@@ -0,0 +1,61 @@
+<markdown>
+  # Linear Gradient
+
+  You may want to use color combinations.
+  </markdown>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { changeColor } from 'seemly'
+import { useThemeVars } from 'naive-ui'
+
+export default defineComponent({
+  setup() {
+    const percentageRef = ref(80)
+    return {
+      percentage: percentageRef,
+      themeVars: useThemeVars(),
+      changeColor
+    }
+  }
+})
+</script>
+
+<template>
+  <n-space vertical>
+    <n-progress
+      type="line"
+      :percentage="percentage"
+      :show-indicator="false"
+      :color="{ from: 'white', to: 'orange' }"
+    />
+    <n-progress
+      type="line"
+      :percentage="percentage"
+      :show-indicator="false"
+      :color="{ from: 'orange', to: 'pink' }"
+    />
+    <n-space>
+      <n-progress
+        type="circle"
+        :percentage="percentage"
+        :color="{ from: 'pink', to: 'red' }"
+      />
+      <n-progress
+        style="width: 120px; margin: 0 8px 12px 0"
+        type="multiple-circle"
+        :stroke-width="10"
+        :circle-gap="10"
+        :percentage="[80, 70]"
+        :color="[
+          { from: 'orange', to: 'pink' },
+          { from: 'pink', to: 'red' },
+        ]"
+        :rail-color="[
+          changeColor(themeVars.infoColor, { alpha: 0.2 }),
+          changeColor(themeVars.infoColor, { alpha: 0.2 }),
+        ]"
+      />
+    </n-space>
+  </n-space>
+</template>
