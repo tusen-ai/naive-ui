03ef069babc3f680df4eeb9f89deeddb5b04fe99
黄博远 <david.2003@foxmail.com>
2024-10-21 19:54:46 +0800

    fix(select): issue 6451, fix showOnFocus attr cause  first click openMenu failed

--- src/select/src/Select.tsx
+++ src/select/src/Select.tsx
@@ -252,6 +252,7 @@ export default defineComponent({
       props,
       mergedClsPrefixRef
     )
+    const preOpenMenu = ref(false)
     const uncontrolledValueRef = ref(props.defaultValue)
     const controlledValueRef = toRef(props, 'value')
     const mergedValueRef = useMergedState(
@@ -443,6 +444,7 @@ export default defineComponent({
     function doFocus(e: FocusEvent): void {
       const { onFocus, showOnFocus } = props
       const { nTriggerFormFocus } = formItem
+      preOpenMenu.value = true
       if (onFocus)
         call(onFocus, e)
       nTriggerFormFocus()
@@ -525,6 +527,10 @@ export default defineComponent({
       }
     }
     function handleTriggerClick(): void {
+      if (preOpenMenu.value) {
+        uncontrolledShowRef.value = false
+        preOpenMenu.value = false
+      }
       if (mergedDisabledRef.value)
         return
       if (!mergedShowRef.value) {
