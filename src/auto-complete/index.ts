00c775c967fd5c322b16647fdce70367dbafc57b
Naily <zero@naily.cc>
2024-12-07 20:39:34 +0800

    feat(auto-complete): export slots type (#6599)

--- src/auto-complete/index.ts
+++ src/auto-complete/index.ts
@@ -1,5 +1,5 @@
 export { autoCompleteProps, default as NAutoComplete } from './src/AutoComplete'
-export type { AutoCompleteProps } from './src/AutoComplete'
+export type { AutoCompleteProps, AutoCompleteSlots } from './src/AutoComplete'
 export type {
   AutoCompleteDefaultSlotOptions,
   AutoCompleteGroupOption,
