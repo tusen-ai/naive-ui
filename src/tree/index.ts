45366cba8bee371014ed0312766f699977261046
Naily <zero@naily.cc>
2024-12-08 00:21:23 +0800

    feat(tree): export slots type (#6599)

--- src/tree/index.ts
+++ src/tree/index.ts
@@ -7,5 +7,5 @@ export type {
   TreeOverrideNodeClickBehaviorReturn
 } from './src/interface'
 export { default as NTree, treeProps } from './src/Tree'
-export type { TreeProps } from './src/Tree'
+export type { TreeProps, TreeSlots } from './src/Tree'
 export { treeGetClickTarget } from './src/utils'
