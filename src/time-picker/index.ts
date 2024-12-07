b18dc5e40eb9040840accdd8eb243d5d3bb0ca74
Naily <zero@naily.cc>
2024-12-08 00:16:54 +0800

    feat(time-picker): export slots type (#6599)

--- src/time-picker/index.ts
+++ src/time-picker/index.ts
@@ -1,3 +1,3 @@
 export type { TimePickerInst } from './src/interface'
 export { default as NTimePicker, timePickerProps } from './src/TimePicker'
-export type { TimePickerProps } from './src/TimePicker'
+export type { TimePickerProps, TimePickerSlots } from './src/TimePicker'
