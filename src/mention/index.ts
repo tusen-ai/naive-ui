bad51fd44f0a52396f5b19e5eb6cf04743918682
Naily <zero@naily.cc>
2024-12-07 23:21:52 +0800

    feat(mention): add slots type (#6599)

--- src/mention/index.ts
+++ src/mention/index.ts
@@ -1,3 +1,3 @@
 export type { MentionInst, MentionOption } from './src/interface'
 export { mentionProps, default as NMention } from './src/Mention'
-export type { MentionProps } from './src/Mention'
+export type { MentionProps, MentionSlots } from './src/Mention'
