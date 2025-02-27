2a51054e01602bebb3d8945c7b73e49d1b7ecf6a
郑伟聪 <1445890792@qq.com>
2025-02-27 14:47:27 +0800

    [f2c16028a] feat: 提交segement组件

---
+++ src/segment/demos/enUS/disabled.demo.vue
@@ -0,0 +1,64 @@
+<markdown>
+# Disabled
+
+Disable the entire component or disable a single option.
+</markdown>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const value1 = ref('Shakermaker')
+const options = ref([
+  {
+    value: 'Rock\'n\'Roll Star',
+    label: 'Rock\'n\'Roll Star',
+    disabled: true
+  },
+  {
+    value: 'Shakermaker',
+    label: 'Shakermaker'
+  },
+  {
+    value: 'Live Forever',
+    label: 'Live Forever',
+    disabled: true
+  },
+  {
+    value: 'Up in the Sky',
+    label: 'Up in the Sky',
+    disabled: true
+  },
+  {
+    value: '...',
+    label: '...',
+    disabled: true
+  }
+])
+
+const value2 = ref('茴')
+const options1 = ref([
+  {
+    label: '茴',
+    value: '茴'
+  },
+  {
+    label: '回',
+    value: '回'
+  },
+  {
+    label: '囘',
+    value: '囘'
+  },
+  {
+    label: '囬',
+    value: '囬'
+  }
+])
+</script>
+
+<template>
+  <n-space vertical>
+    <n-segment v-model:value="value1" :options="options" />
+    <n-segment v-model:value="value2" :options="options1" disabled />
+  </n-space>
+</template>
