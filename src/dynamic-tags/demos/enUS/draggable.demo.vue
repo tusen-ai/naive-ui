059fad7bdd5472a79dd4a997116e37810e83cfbc
郑伟聪 <1445890792@qq.com>
2025-03-25 14:02:50 +0800

    feat: 提交tags拖拽功能

---
+++ src/dynamic-tags/demos/enUS/draggable.demo.vue
@@ -0,0 +1,25 @@
+<markdown>
+# draggable
+</markdown>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const tags = ref([
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September'
+])
+</script>
+
+<template>
+  <n-space>
+    <n-dynamic-tags v-model:value="tags" draggable />
+  </n-space>
+</template>
--- src/dynamic-tags/src/styles/index.cssr.ts
+++ src/dynamic-tags/src/styles/index.cssr.ts
@@ -5,5 +5,13 @@ import { cB } from '../../../_utils/cssr'
 export default cB('dynamic-tags', [
   cB('input', {
     minWidth: 'var(--n-input-width)'
-  })
+  }),
+  cB('dynamic-tag-move', `
+    user-select: none;
+  `, [
+    cB('tag', `
+      cursor: move;
+      transition: unset;
+    `)
+  ])
 ])
