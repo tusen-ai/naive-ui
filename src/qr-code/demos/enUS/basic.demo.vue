4701c334a77d3fc68b8ab4aa6ac7457ceed0f2a5
jahnli <jahnli@163.com>
2025-06-21 23:01:35 +0800

    feat(n-qr-code): change to setup API

--- src/qr-code/demos/enUS/basic.demo.vue
+++ src/qr-code/demos/enUS/basic.demo.vue
@@ -4,17 +4,10 @@
   A basic QR Code.
 </markdown>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-export default defineComponent({
-  setup() {
-    const text = ref('The rain dampened the sky')
-    return {
-      text
-    }
-  }
-})
+const text = ref('The rain dampened the sky')
 </script>
 
 <template>
--- src/qr-code/demos/enUS/size.demo.vue
+++ src/qr-code/demos/enUS/size.demo.vue
@@ -2,37 +2,26 @@
   # Size
 </markdown>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-export default defineComponent({
-  setup() {
-    const text = ref('The rain dampened the sky')
+const text = ref('The rain dampened the sky')
 
-    const size = ref(110)
+const size = ref(110)
 
-    const add = () => {
-      size.value += 10
-      if (size.value > 200) {
-        size.value = 110
-      }
-    }
-
-    const minus = () => {
-      size.value -= 10
-      if (size.value < 20) {
-        size.value = 110
-      }
-    }
-
-    return {
-      size,
-      text,
-      add,
-      minus
-    }
+function add() {
+  size.value += 10
+  if (size.value > 200) {
+    size.value = 110
   }
-})
+}
+
+function minus() {
+  size.value -= 10
+  if (size.value < 20) {
+    size.value = 110
+  }
+}
 </script>
 
 <template>
