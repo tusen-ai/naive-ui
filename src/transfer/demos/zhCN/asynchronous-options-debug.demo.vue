c9cb1eacfa10160e9e770386fbe6fc15948c4da0
jahnli <jahnli@163.com>
2025-06-21 13:43:09 +0800

    feat(n-transfer): change to setup API

--- src/transfer/demos/zhCN/asynchronous-options-debug.demo.vue
+++ src/transfer/demos/zhCN/asynchronous-options-debug.demo.vue
@@ -2,39 +2,31 @@
 # 异步options报错debug
 </markdown>
 
-<script lang="ts">
+<script lang="ts" setup>
 import type { TransferOption } from 'naive-ui'
-import { defineComponent, ref } from 'vue'
+import { ref } from 'vue'
 
-export default defineComponent({
-  setup() {
-    const values = ref([1])
-    const options = ref<TransferOption[]>([])
-    const getOptions = () => {
-      setTimeout(() => {
-        options.value = [
-          {
-            label: 'Option1',
-            value: 1
-          },
-          {
-            label: 'Option2',
-            value: 2
-          },
-          {
-            label: 'Option3',
-            value: 3
-          }
-        ]
-      }, 1000)
-    }
-    getOptions()
-    return {
-      options,
-      values
-    }
-  }
-})
+const values = ref([1])
+const options = ref<TransferOption[]>([])
+function getOptions() {
+  setTimeout(() => {
+    options.value = [
+      {
+        label: 'Option1',
+        value: 1
+      },
+      {
+        label: 'Option2',
+        value: 2
+      },
+      {
+        label: 'Option3',
+        value: 3
+      }
+    ]
+  }, 1000)
+}
+getOptions()
 </script>
 
 <template>
