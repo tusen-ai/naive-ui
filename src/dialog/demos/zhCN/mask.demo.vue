dc0d82405d2d29a5279be1e84cab6d6365103f9b
jahnli <jahnli@163.com>
2025-06-23 11:42:44 +0800

    feat(n-dialog): change to setup API

--- src/dialog/demos/zhCN/mask.demo.vue
+++ src/dialog/demos/zhCN/mask.demo.vue
@@ -4,34 +4,27 @@
 我觉得用户应该聪明到点遮罩没用的时候就去点确认了。
 </markdown>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { useDialog, useMessage } from 'naive-ui'
-import { defineComponent } from 'vue'
 
-export default defineComponent({
-  setup() {
-    const message = useMessage()
-    const dialog = useDialog()
+const message = useMessage()
+const dialog = useDialog()
 
-    return {
-      handleButtonClick() {
-        dialog.success({
-          title: '关闭',
-          content: '你确定？',
-          positiveText: '确定',
-          negativeText: '不确定',
-          maskClosable: false,
-          onMaskClick: () => {
-            message.success('不能关闭')
-          },
-          onEsc: () => {
-            message.success('通过 esc 关闭')
-          }
-        })
-      }
+function handleButtonClick() {
+  dialog.success({
+    title: '关闭',
+    content: '你确定？',
+    positiveText: '确定',
+    negativeText: '不确定',
+    maskClosable: false,
+    onMaskClick: () => {
+      message.success('不能关闭')
+    },
+    onEsc: () => {
+      message.success('通过 esc 关闭')
     }
-  }
-})
+  })
+}
 </script>
 
 <template>
