95037766813214f63d37755c0c098a5af61128aa
jahnli <jahnli@163.com>
2025-06-29 17:55:36 +0800

    feat(code): change to setup API (#6972)

--- src/code/demos/enUS/softwrap.demo.vue
+++ src/code/demos/enUS/softwrap.demo.vue
@@ -4,21 +4,13 @@
 Code can wrap content if it overflows.
 </markdown>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  setup() {
-    return {
-      code: `
+<script lang="ts" setup>
+const code = `
 function padEnd(string, length, chars) {
   const strLength = length ? stringSize(string) : 0
   return (length && strLength < length)? (string + createPadding(length - strLength, chars)): (string || '')
 }
       `
-    }
-  }
-})
 </script>
 
 <template>
