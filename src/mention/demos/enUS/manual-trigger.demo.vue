10ded281ad1224f5b57337496a286853b64baffc
jahnli <jahnli@163.com>
2025-06-29 18:02:42 +0800

    feat(input-mention): change to setup API (#6957)

--- src/mention/demos/enUS/manual-trigger.demo.vue
+++ src/mention/demos/enUS/manual-trigger.demo.vue
@@ -4,56 +4,50 @@
 You may want to trigger `focus` and `blur` manually?
 </markdown>
 
-<script lang="ts">
+<script lang="ts" setup>
 import type { MentionInst, MentionOption } from 'naive-ui'
 import type { VNodeChild } from 'vue'
 import { HomeOutline as HomeIcon } from '@vicons/ionicons5'
 import { NIcon } from 'naive-ui'
-import { defineComponent, h, ref } from 'vue'
+import { h, ref } from 'vue'
 
-export default defineComponent({
-  setup() {
-    const myMentionRef = ref<MentionInst | null>(null)
-    const triggerBlur = () => {
-      myMentionRef.value?.blur()
-    }
-    const triggerFocus = () => {
-      myMentionRef.value?.focus()
-      setTimeout(triggerBlur, 1000)
-    }
-    return {
-      myMention: myMentionRef,
-      triggerFocus,
-      triggerBlur,
-      options: [
-        {
-          label: '07akioni',
-          value: '07akioni'
-        },
-        {
-          label: 'star-kirby',
-          value: 'star-kirby'
-        },
-        {
-          label: 'Guandong-Road',
-          value: 'Guandong-Road'
-        },
-        {
-          label: (option: MentionOption): VNodeChild =>
-            h('span', { style: 'display: flex; align-items: center;' }, [
-              h(
-                NIcon,
-                { style: 'margin-right: 5px' },
-                { default: () => h(HomeIcon) }
-              ),
-              option.value
-            ]),
-          value: 'No.5-Yiheyuan-Road'
-        }
-      ]
-    }
+const myMention = ref<MentionInst | null>(null)
+
+function triggerBlur() {
+  myMention.value?.blur()
+}
+
+function triggerFocus() {
+  myMention.value?.focus()
+  setTimeout(triggerBlur, 1000)
+}
+
+const options = [
+  {
+    label: '07akioni',
+    value: '07akioni'
+  },
+  {
+    label: 'star-kirby',
+    value: 'star-kirby'
+  },
+  {
+    label: 'Guandong-Road',
+    value: 'Guandong-Road'
+  },
+  {
+    label: (option: MentionOption): VNodeChild =>
+      h('span', { style: 'display: flex; align-items: center;' }, [
+        h(
+          NIcon,
+          { style: 'margin-right: 5px' },
+          { default: () => h(HomeIcon) }
+        ),
+        option.value
+      ]),
+    value: 'No.5-Yiheyuan-Road'
   }
-})
+]
 </script>
 
 <template>
--- src/mention/demos/zhCN/basic.demo.vue
+++ src/mention/demos/zhCN/basic.demo.vue
@@ -4,45 +4,39 @@
 如果 `label` 是回调函数，输入匹配则会根据 `value` 进行匹配
 </markdown>
 
-<script lang="ts">
+<script lang="ts" setup>
 import type { MentionOption } from 'naive-ui'
 import type { VNodeChild } from 'vue'
 import { TelescopeOutline } from '@vicons/ionicons5'
 import { NIcon } from 'naive-ui'
-import { defineComponent, h } from 'vue'
+import { h } from 'vue'
 
-export default defineComponent({
-  setup() {
-    return {
-      options: [
-        {
-          label: '07akioni',
-          value: '07akioni'
-        },
-        {
-          label: 'star-kirby',
-          value: 'star-kirby'
-        },
-        {
-          label: '广东路',
-          value: '广东路'
-        },
-        {
-          label: (option: MentionOption): VNodeChild =>
-            h('div', { style: 'display: flex; align-items: center;' }, [
-              h(NIcon, {
-                style: 'margin-right: 6px;',
-                size: 18,
-                component: TelescopeOutline
-              }),
-              option.value
-            ]),
-          value: '颐和园路5号'
-        }
-      ]
-    }
+const options = [
+  {
+    label: '07akioni',
+    value: '07akioni'
+  },
+  {
+    label: 'star-kirby',
+    value: 'star-kirby'
+  },
+  {
+    label: '广东路',
+    value: '广东路'
+  },
+  {
+    label: (option: MentionOption): VNodeChild =>
+      h('div', { style: 'display: flex; align-items: center;' }, [
+        h(NIcon, {
+          style: 'margin-right: 6px;',
+          size: 18,
+          component: TelescopeOutline
+        }),
+        option.value
+      ]),
+    value: '颐和园路5号'
   }
-})
+]
 </script>
 
 <template>
