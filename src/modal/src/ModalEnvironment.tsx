fa2cc4d606916aa67a238feddbbc36cc9bcb557d
07akioni <07akioni2@gmail.com>
2025-09-13 14:26:03 +0800

    refactor(modal): clean useModal render PR code

--- src/modal/src/ModalEnvironment.tsx
+++ src/modal/src/ModalEnvironment.tsx
@@ -117,9 +117,7 @@ export const NModalEnvironment = defineComponent({
         internalAppear
         internalModal
       >
-        {{
-          default: this.$slots.default
-        }}
+        {this.$slots}
       </NModal>
     )
   }
